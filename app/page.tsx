c9161cf2a6e1d98c0a30bf8aa2a7e3d59d1f415b
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-07-22 17:49:07 +0900

    wrapping Image to Status, Icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Status } from "@/components/Status";
+import { Icon } from "@/components/Icon/Icon";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static  lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 ">
                     Get started by editing&nbsp;
                     <code className="font-mono font-bold">app/page.tsx</code>
                 </p>
@@ -15,25 +17,16 @@ export default function Home() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <Image
-                            src="/status/idle.svg"
-                            alt="idle state"
-                            width={24}
-                            height={24}
-                            priority
-                        />
+                        <Status type="do-not-disturb" size={200} />
                     </a>
                 </div>
             </div>
 
             <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-slate-300 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/doubleSharp.svg"
-                    alt="Double Sharp Pong Logo"
-                    width={180}
-                    height={180}
-                    priority
+                <Icon
+                    className="relative flex dark:drop-shadow-sm dark:invert"
+                    type="doubleSharp"
+                    size={240}
                 />
             </div>
 
