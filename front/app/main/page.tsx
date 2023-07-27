9df0bcf2fdf3953724ca4ae961e8bc6d0ac17b96
chanhpar <chanhpar@student.42seoul.kr>
2023-07-27 16:47:35 +0900

    fix import: Chatting -> Chat

--- front/app/main/page.tsx
+++ front/app/main/page.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { ChatLayout } from "@/components/Chatting/ChatLayout";
+import { ChatLayout } from "@/components/Chat/ChatLayout";
 import { NavigationBar } from "@/components/NavigationBar/NavigationBar";
 
 export default function Main() {
