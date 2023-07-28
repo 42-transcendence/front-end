70653abb3a8e71a453ab88239c45d9b88a9c39d5
El <72256725+ElApostles@users.noreply.github.com>
2023-07-28 18:36:29 +0900

    remove extra

--- front/app/main/@main/dropdown/page.tsx
+++ front/app/main/@main/dropdown/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import { Status } from "@/components/Status";
-import { Icon } from "@/components/Icon/Icon";
-import { Avatar } from "@/components/Avatar";
 import { FriendModal } from "@/components/FriendModal";
 
 export default function Home() {
@@ -11,5 +7,3 @@ export default function Home() {
         </main>
     );
 }
-
-// border-transparent gradient-border before:pointer-elvents-none relative flex w-[262px] flex-col items-start rounded-[28px] bg-windowGlass/30 p-px backdrop-blur-[20px] backdrop-brightness-100 before:absolute before:inset-0 before:rounded-[28px] before:p-px before:content-[''] hover:bg-primary hover:dark:border-purple-700 hover:dark:bg-neutral-800/30 active:bg-violet-700 focus:outline-gray-100 focus:ring focus:ring-violet-300 group border px-5 py-4 transition-colors gap-2
