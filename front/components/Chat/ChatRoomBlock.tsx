a99d9f8262d6c4f3a1e6605ab779f495ed381fd0
Yui <yui@Yui-Master.local>
2023-07-30 16:58:34 +0900

    Add key to map; add block

--- front/components/Chat/ChatRoomBlock.tsx
+++ front/components/Chat/ChatRoomBlock.tsx
@@ -7,13 +7,13 @@ import { useState } from "react";
 type AvatarProp = {
     profileImage: string;
     userStatus:
-    | "online"
-    | "invisible"
-    | "offline"
-    | "idle"
-    | "matching"
-    | "do-not-disturb"
-    | "in-game";
+        | "online"
+        | "invisible"
+        | "offline"
+        | "idle"
+        | "matching"
+        | "do-not-disturb"
+        | "in-game";
 };
 
 const dummyAvatar: AvatarProp = {
@@ -21,7 +21,9 @@ const dummyAvatar: AvatarProp = {
     userStatus: "in-game",
 };
 
-export default function ChatRoomBlock({ chatRoomInfo }: {
+export default function ChatRoomBlock({
+    chatRoomInfo,
+}: {
     chatRoomInfo: {
         id: number;
         members: string[];
@@ -30,7 +32,7 @@ export default function ChatRoomBlock({ chatRoomInfo }: {
         nonReadYetMsgNum: number;
         title?: string;
         isLocked: boolean;
-    }
+    };
 }) {
     // const [messageCount, setMessageCount] = useState(1);
 
@@ -39,21 +41,20 @@ export default function ChatRoomBlock({ chatRoomInfo }: {
     // }
 
     /* 숫자 많으면 +로 바뀌도록 처리하기 (아주 나중이 될듯 ㅋㅋ api언제만듬~~) */
-    const nonReadYetMsgNum = chatRoomInfo.nonReadYetMsgNum > 999 ? (
-        <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
-            <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
-                999+
+    const nonReadYetMsgNum =
+        chatRoomInfo.nonReadYetMsgNum > 999 ? (
+            <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
+                <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
+                    999+
+                </div>
             </div>
-        </div>
-    )
-        : chatRoomInfo.nonReadYetMsgNum > 0 ? (
+        ) : chatRoomInfo.nonReadYetMsgNum > 0 ? (
             <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
                 <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
                     {chatRoomInfo.nonReadYetMsgNum.toString()}
                 </div>
             </div>
-        )
-            : null;
+        ) : null;
 
     return (
         <>
@@ -78,15 +79,18 @@ export default function ChatRoomBlock({ chatRoomInfo }: {
                 <div className="flex h-16 flex-[1_0_0] items-center justify-between overflow-hidden py-2 pl-0 pr-3">
                     <div className="flex flex-col items-start">
                         <div className="inilne-block text-sans h-4 max-w-[160px] text-[17px] font-normal not-italic leading-6 text-[color:var(--labels-primary-dark,#FFF)]">
-                            <span className="pr-2 truncate">
-                                {chatRoomInfo.title ?? chatRoomInfo.members.toString()}
+                            <span className="block truncate pr-2">
+                                {chatRoomInfo.title ??
+                                    chatRoomInfo.members.toString()}
                             </span>
                             <span className="top-[2.5px]e absolute items-center space-x-1">
-                                {chatRoomInfo.isLocked ? <Icon
-                                    className="inline-block "
-                                    type="lock"
-                                    size={20}
-                                /> : null}
+                                {chatRoomInfo.isLocked ? (
+                                    <Icon
+                                        className="inline-block "
+                                        type="lock"
+                                        size={20}
+                                    />
+                                ) : null}
                                 <span className="text-sans overflow-hidden text-ellipsis text-[13px] font-normal not-italic leading-[19px] text-[color:var(--text-tertiary,rgba(255,255,255,0.11))]">
                                     {chatRoomInfo.members.length}
                                 </span>
@@ -100,7 +104,8 @@ export default function ChatRoomBlock({ chatRoomInfo }: {
 
                     <div className="flex flex-col items-end gap-2 self-stretch">
                         <div className="text-sans overflow-hidden text-ellipsis text-right text-[13px] font-normal not-italic leading-[18px] text-[color:var(--text-secondary,rgba(255,255,255,0.23))]">
-                            12월 30일 {/* TODO: chatRoomInfo.latestMessage.date */}
+                            12월 30일{" "}
+                            {/* TODO: chatRoomInfo.latestMessage.date */}
                         </div>
                         {nonReadYetMsgNum}
                     </div>
--- front/components/Chat/ChatSideBar.tsx
+++ front/components/Chat/ChatSideBar.tsx
@@ -11,48 +11,48 @@ const chatRoomsDummy: {
     title?: string;
     isLocked: boolean;
 }[] = [
-        {
-            id: 1,
-            members: ["hdoo", "hdoo", "chanhpar", "iyun", "jkong"],
-            tag: "#000011",
-            latestMessage: "맛있는 돈까스가 먹고싶어요 난 등심이 좋더라..",
-            nonReadYetMsgNum: 0,
-            isLocked: true,
-        },
-        {
-            id: 2,
-            members: ["chanhpar"],
-            tag: "#000012",
-            latestMessage: "옹옹엉양ㄹ오라ㅣㅁㄴ오맂다넝로미어ㅏ로미단로이머니",
-            nonReadYetMsgNum: 10,
-            isLocked: true,
-            title: "Title!",
-        },
-        {
-            id: 3,
-            members: ["iyun"],
-            tag: "#000013",
-            latestMessage: "I'm IU ,,>ㅅ<,,",
-            nonReadYetMsgNum: 120,
-            isLocked: false,
-        },
-        {
-            id: 4,
-            members: ["jkong"],
-            tag: "#000014",
-            latestMessage: "I'm Jkong!",
-            nonReadYetMsgNum: 3,
-            isLocked: true,
-        },
-        {
-            id: 5,
-            members: ["jisookim"],
-            tag: "#000015",
-            latestMessage: "Hi I'm jisoo",
-            nonReadYetMsgNum: 1029,
-            isLocked: false,
-        },
-    ];
+    {
+        id: 1,
+        members: ["hdoo", "hdoo", "chanhpar", "iyun", "jkong"],
+        tag: "#000011",
+        latestMessage: "맛있는 돈까스가 먹고싶어요 난 등심이 좋더라..",
+        nonReadYetMsgNum: 0,
+        isLocked: true,
+    },
+    {
+        id: 2,
+        members: ["chanhpar"],
+        tag: "#000012",
+        latestMessage: "옹옹엉양ㄹ오라ㅣㅁㄴ오맂다넝로미어ㅏ로미단로이머니",
+        nonReadYetMsgNum: 10,
+        isLocked: true,
+        title: "Title!",
+    },
+    {
+        id: 3,
+        members: ["iyun"],
+        tag: "#000013",
+        latestMessage: "I'm IU ,,>ㅅ<,,",
+        nonReadYetMsgNum: 120,
+        isLocked: false,
+    },
+    {
+        id: 4,
+        members: ["jkong"],
+        tag: "#000014",
+        latestMessage: "I'm Jkong!",
+        nonReadYetMsgNum: 3,
+        isLocked: true,
+    },
+    {
+        id: 5,
+        members: ["jisookim"],
+        tag: "#000015",
+        latestMessage: "Hi I'm jisoo",
+        nonReadYetMsgNum: 1029,
+        isLocked: false,
+    },
+];
 
 function ChatSideBarHeader() {
     return (
@@ -71,35 +71,37 @@ function ChatSideBarHeader() {
 //TODO name ChatRoomSearch, SearchChatRoom, + new ChatRoom Button..?
 function ChatRoomSearch() {
     return (
-        <div className="rounded-xl; flex h-8 shrink-0 items-center gap-2 self-stretch px-2 py-0" >
+        <div className="rounded-xl; flex h-8 shrink-0 items-center gap-2 self-stretch px-2 py-0">
             <div className="flex h-8 w-[305px] shrink-0 items-center gap-2 self-stretch rounded-xl px-2 py-0 shadow-[1px_1.5px_4px_0px_rgba(0,0,0,0.10)_inset,1px_1.5px_4px_0px_rgba(0,0,0,0.08)_inset,0px_-0.5px_1px_0px_rgba(255,255,255,0.25)_inset,0px_-0.5px_1px_0px_rgba(255,255,255,0.30)_inset]">
                 {/* <Icon className="float-left" type="search" size={30} /> */}
                 <div className="flex-[1_0_0] text-sm font-normal not-italic leading-[22px] text-[color:var(--text-secondary,rgba(255,255,255,0.23))]">
-                    <input type="text" className="w-[260px] overflow-hidden text-ellipsis border-[none] bg-transparent outline-none"></input>
+                    <input
+                        type="text"
+                        className="w-[260px] overflow-hidden text-ellipsis border-[none] bg-transparent outline-none"
+                    ></input>
                 </div>
                 <button>
-                    <Icon
-                        className="float-left"
-                        type="search"
-                        size={30}
-                    />
+                    <Icon className="float-left" type="search" size={30} />
                 </button>
             </div>
-        </div >
+        </div>
     );
 }
 
 export default function ChatSideBar() {
-
     const chatRoomList = chatRoomsDummy; // TODO: get chatRoomList from server
-    const chatRoomBlocklist = chatRoomList.map((chatRoomInfo) => <ChatRoomBlock chatRoomInfo={chatRoomInfo} />);
+    const chatRoomBlocklist = chatRoomList.map((chatRoomInfo) => (
+        <ChatRoomBlock key={chatRoomInfo.id} chatRoomInfo={chatRoomInfo} />
+    ));
 
     return (
         <div>
             <div className="float-left flex h-full shrink-0 flex-col items-start gap-2 rounded-[0px_28px_28px_0px] p-4 backdrop-blur-[50px]">
                 <ChatSideBarHeader />
                 <ChatRoomSearch />
-                <> {/* div같은걸로 감싸는게 안낫나요? */}
+                <>
+                    {" "}
+                    {/* div같은걸로 감싸는게 안낫나요? */}
                     {chatRoomBlocklist}
                 </>
             </div>
