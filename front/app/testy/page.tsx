4d33c21203e4075f60f5d5f9fc5e5a1dc5a5c9d7
jisookim <nimmo9040@gmail.com>
2023-08-01 19:48:25 +0900

    Game 컴포넌트 파일 생성, AchivementCard 구현, 트로피 그림 추가

--- front/app/testy/page.tsx
+++ front/app/testy/page.tsx
@@ -1,9 +1,13 @@
-import ChatRoomBlock from "@/components/Chat/Chat";
+import AchivementCard from "@/components/Game/AchivementCard";
+import GameNavBar from "@/components/Game/GameNavBar";
+import NotificationCard from "@/components/Game/NotificationCard";
+import ResultCard from "@/components/Game/ResultCard";
 
 export default function Main() {
     return (
         <div>
-            <ChatRoomBlock />
+            {/* <AchivementCard/> */}
+            <ResultCard/>
         </div>
     );
 }
