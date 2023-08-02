888e54e058ef15907cfabad08d93fe8d0b992dcc
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-03 02:18:22 +0900

    logo position adjust

--- front/app/main/@login/page.tsx
+++ front/app/main/@login/page.tsx
@@ -6,15 +6,15 @@ import DoubleSharp from "/public/doubleSharp.svg";
 export default function LoginPage() {
     return (
         <section>
-            <div className="relative flex h-screen w-screen flex-col items-center justify-center gap-20">
+            <div className="relative flex h-screen w-screen flex-col items-center justify-center ">
                 <div className="relative flex h-full items-end justify-center">
                     <DoubleSharp
-                        className="h-fit w-fit animate-spin-slow text-black drop-shadow-[0_0_3rem_#00000050] delay-300 dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                        className="relative h-fit w-fit animate-spin-slow text-black drop-shadow-[0_0_3rem_#00000050] delay-300 dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                         width={130}
                         height="100%"
                     />
                 </div>
-                <div className="relative flex h-[50%] w-fit flex-col items-center justify-center gap-1 py-10">
+                <div className="relative flex h-[90%] w-fit flex-col items-center justify-center gap-1">
                     <GoogleLoginButton />
                     <FtLoginButton />
                 </div>
