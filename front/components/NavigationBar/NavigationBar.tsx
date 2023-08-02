67c8833c751ec886acddad4bb37ab02e65ea9ef5
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-03 05:10:43 +0900

    Design change: Social icon to Chat, make fill when modal is up

--- front/components/NavigationBar/NavigationBar.tsx
+++ front/components/NavigationBar/NavigationBar.tsx
@@ -7,13 +7,11 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 import React, { useState } from "react";
 import { createPortal } from "react-dom";
 import Link from "next/link";
-import "./style.css";
-import { Icon } from "../Icon/Icon";
 import { Avatar } from "../Avatar";
 import { ChatLayout } from "../Chat/ChatLayout";
-import Image from "next/image";
 import DoubleSharp from "/public/doubleSharp.svg";
-import Social from "/public/social.svg";
+import Chat from "/public/chat.svg";
+import Chatfilled from "/public/chatfilled.svg";
 
 function SocialButton() {
     const [showModal, setShowModal] = useState(false);
@@ -24,20 +22,22 @@ function SocialButton() {
     if (mainNode === null) {
         return (
             <button type="button" onClick={handleClick}>
-                <Social
-                    className="text-white drop-shadow-[0_0_0.3rem_#00000030] dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-                    width={42}
+                <Chat
+                    className="drop-shadow-[0_0_3rem_#ffffff30] dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                    width={32}
                     height="100%"
                 />
             </button>
         );
     }
 
+    const ChatIcon = showModal ? Chatfilled : Chat;
+
     return (
         <>
-            <button type="button" onClick={handleClick}>
-                <Social
-                    className="text-white drop-shadow-[0_0_0.3rem_#00000030] dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            <button type="button" className="" onClick={handleClick}>
+                <ChatIcon
+                    className="text-black drop-shadow-[0_0_0.3rem_#00000070] focus:bg-controlsSelected dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                     width={42}
                     height="100%"
                 />
@@ -59,16 +59,10 @@ export function NavigationBar({}): React.ReactElement {
 
     return (
         <>
-            <div className="relative flex h-16 w-full flex-row items-center justify-between bg-neutral-900/30 px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100">
+            <div className="relative flex h-16 w-full flex-row items-center justify-between bg-darker px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100 dark:bg-priVar/30">
                 <Link className="relative" href="/main">
-                    {/* <Icon
-                        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                        type="doubleSharp"
-                        size={20}
-                    /> */}
-
                     <DoubleSharp
-                        className="text-black drop-shadow-[0_0_0.3rem_#00000030] dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                        className="text-black drop-shadow-[0_0_0.3rem_#00000070] dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                         width={32}
                         height="100%"
                     />
@@ -77,9 +71,9 @@ export function NavigationBar({}): React.ReactElement {
                     <SocialButton />
                     {/* TODO: on click Avatar in navbar, show context menu myinfo */}
                     <Avatar
-                        size={"w-12"}
+                        size={"w-10"}
                         accountId={1}
-                        className={"fill-ultraDark"}
+                        className={"bg-white/30 dark:bg-black/30"}
                     />
                 </div>
             </div>
