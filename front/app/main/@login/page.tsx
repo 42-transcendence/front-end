c1abd2e0b3bfa9d26bae8b579063f6e414e5810b
hdoo <tyeirdoo06@gmail.com>
2023-08-02 13:21:52 +0900

    adjust login button position

--- front/app/main/@login/page.tsx
+++ front/app/main/@login/page.tsx
@@ -6,7 +6,7 @@ import DoubleSharp from "/public/doubleSharp.svg";
 export default function LoginPage() {
     return (
         <section>
-            <div className="relative flex h-screen w-screen flex-col items-center justify-center gap-20 py-10">
+            <div className="relative flex h-screen w-screen flex-col items-center justify-center gap-20">
                 <div className="relative flex h-full items-end justify-center">
                     <DoubleSharp
                         className="h-fit w-fit animate-spin-slow text-black drop-shadow-[0_0_3rem_#00000050] delay-300 dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
@@ -14,7 +14,7 @@ export default function LoginPage() {
                         height="100%"
                     />
                 </div>
-                <div className="relative flex h-[50%] w-fit flex-col items-center justify-start gap-1 py-10">
+                <div className="relative flex h-[50%] w-fit flex-col items-center justify-center gap-1 py-10">
                     <GoogleLoginButton />
                     <FtLoginButton />
                 </div>
