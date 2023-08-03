52059c33304140b30fd8c77d2c7690b122c25091
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-03 21:45:17 +0900

    Fix layout of game main page

--- front/components/Chat/ChatSideBar.tsx
+++ front/components/Chat/ChatSideBar.tsx
@@ -65,7 +65,7 @@ export default function ChatSideBar() {
                 </div>
 
                 {/* searchBar */}
-                <div className="flex h-8 shrink-0 items-center gap-2 self-stretch rounded-xl bg-black/30 px-2 py-0 shadow-3xl">
+                <div className="shadow-3xl flex h-8 shrink-0 items-center gap-2 self-stretch rounded-xl bg-black/30 px-2 py-0">
                     <div className="flex h-8 w-[305px] shrink-0 items-center gap-2 self-stretch rounded-xl px-2 py-0 ">
                         {/* <Icon className="float-left" type="search" size={30} /> */}
                         <div className="flex-[1_0_0] overflow-hidden text-ellipsis text-sm font-normal not-italic leading-[22px] text-[color:var(--text-secondary,rgba(255,255,255,0.23))]">
@@ -80,16 +80,13 @@ export default function ChatSideBar() {
                         </button>
                     </div>
                 </div>
-
-                {/* {chatRoomsDummy.map((elem) => (
-
-                    <ChatRoomBlock key={elem.id} {...elem}/>
-                ))} */}
-
-                <ChatRoomBlock />
-                <ChatRoomBlock />
-                <ChatRoomBlock />
-                <ChatRoomBlock />
+                <button>
+                    <SearchIcon
+                        className="text-gray-50"
+                        width={24}
+                        height="100%"
+                    />
+                </button>
             </div>
         </div>
     );
