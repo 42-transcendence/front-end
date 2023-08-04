ff87c62c28a5f107da37923498bbca472a7e972d
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-04 13:06:44 +0900

    extract text field

--- front/app/main/@main/dropdown/page.tsx
+++ front/app/main/@main/dropdown/page.tsx
@@ -2,7 +2,7 @@ import { FriendModal } from "@/components/FriendModal";
 
 export default function Home() {
     return (
-        <main className="min flex flex-col items-center justify-between p-24">
+        <main className="min flex h-full flex-col items-center justify-between p-24">
             <FriendModal />
         </main>
     );
