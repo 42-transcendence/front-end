b9ec9ca28d48f7d37b98066188bdaecb1566faf2
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-04 17:46:04 +0900

    Replace room list mockups with code

--- front/components/Chat/ChatRoomBlock.tsx
+++ front/components/Chat/ChatRoomBlock.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import LockIcon from "/public/lock.svg";
 import Image from "next/image";
 import { useState } from "react";
+import { ChatRoomInfo } from "./ChatSideBar";
+import { Avatar } from "../Avatar";
 
 type AvatarProp = {
     profileImage: string;
@@ -16,17 +18,44 @@ type AvatarProp = {
         | "in-game";
 };
 
+function getRoomDisplayTitle(chatRoom: ChatRoomInfo) {
+    return chatRoom.title
+        ? chatRoom.title
+        : chatRoom.members
+            .reduce((acc, member) => [...acc, member.name], [] as string[])
+            .join(", ");
+}
+
 const dummyAvatar: AvatarProp = {
     profileImage: "/jisookim.png",
     userStatus: "in-game",
 };
 
-export default function ChatRoomBlock() {
+export default function ChatRoomBlock({
+    chatRoom,
+}: {
+    chatRoom: ChatRoomInfo;
+}) {
     const [messageCount, setMessageCount] = useState(1);
 
     function addMessageCount() {
         setMessageCount(messageCount + 1);
     }
+
+    const numberOfUnreadMessages =
+        chatRoom.numberOfUnreadMessages > 999 ? (
+            <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
+                <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
+                    999+
+                </div>
+            </div>
+        ) : chatRoom.numberOfUnreadMessages > 0 ? (
+            <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
+                <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
+                    {chatRoom.numberOfUnreadMessages.toString()}
+                </div>
+            </div>
+        ) : null;
 
     return (
         <>
@@ -37,12 +66,10 @@ export default function ChatRoomBlock() {
             <div className="flex h-fit shrink-0 items-center gap-4 self-stretch overflow-hidden">
                 <div className="flex items-center justify-center gap-2.5">
                     <div className="flex h-12 w-12 shrink-0 flex-col items-center justify-center rounded-2xl bg-slate-600">
-                        <Image
-                            className=""
-                            src={dummyAvatar.profileImage}
-                            alt="Avatar"
-                            width="43"
-                            height="43"
+                        <Avatar
+                            className={""}
+                            size={"20"}
+                            accountId={chatRoom.members[0].id}
                         />
                     </div>
                 </div>
@@ -50,22 +77,24 @@ export default function ChatRoomBlock() {
                 {/* chatrooms - info */}
                 <div className="flex h-16 flex-[1_0_0] items-center justify-between overflow-hidden py-2 pl-0 pr-3">
                     <div className="flex flex-col items-start">
-                        <div className="inilne-block text-sans h-4 text-ellipsis text-[17px] font-normal not-italic leading-6 text-[color:var(--labels-primary-dark,#FFF)]">
-                            <span className="pr-2">Title!</span>
-                            <span className="top-[2.5px]e absolute items-center space-x-1">
+                        <div className="text-sans flex h-4 flex-row text-ellipsis text-[17px] font-normal not-italic leading-6 text-white/80">
+                            <span className="truncate text-ellipsis pr-2">
+                                {getRoomDisplayTitle(chatRoom)}
+                            </span>
+                            <span className="top-[2.5px] items-center space-x-1">
                                 <LockIcon
-                                    width={15}
+                                    width={12}
                                     height="100%"
-                                    className="text-gray-50"
+                                    className="text-gray-200"
                                 />
                                 <span className="text-sans overflow-hidden text-ellipsis text-[13px] font-normal not-italic leading-[19px] text-[color:var(--text-tertiary,rgba(255,255,255,0.11))]">
-                                    4
+                                    {chatRoom.members.length}
                                 </span>
                             </span>
                         </div>
 
                         <div className="text-sans h-8 max-w-[160px] overflow-hidden truncate text-[13px] font-normal not-italic text-white/30">
-                            돈까스가 먹고싶어요 맛있는 돈까스 등심돈까스
+                            {chatRoom.latestMessage ?? ""}
                         </div>
                     </div>
 
@@ -73,12 +102,7 @@ export default function ChatRoomBlock() {
                         <div className="text-sans overflow-hidden text-ellipsis text-right text-[13px] font-normal not-italic leading-[18px] text-[color:var(--text-secondary,rgba(255,255,255,0.23))]">
                             12월 30일
                         </div>
-                        <div className="flex flex-col items-center justify-center rounded-[100px] bg-red-500 p-0.5 px-1">
-                            <div className="flex text-center text-[12px] text-base font-thin not-italic leading-[normal] text-white ">
-                                {/* 숫자 많으면 +로 바뀌도록 처리하기 (아주 나중이 될듯 ㅋㅋ api언제만듬~~) */}
-                                999+
-                            </div>
-                        </div>
+                        {numberOfUnreadMessages}
                     </div>
                 </div>
             </div>
