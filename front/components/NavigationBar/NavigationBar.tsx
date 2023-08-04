a5350f2e81106c8f2e0d6723b807ccf3b4131dc6
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-04 17:45:02 +0900

    Align the fallback's CSS with the default one

--- front/components/NavigationBar/NavigationBar.tsx
+++ front/components/NavigationBar/NavigationBar.tsx
@@ -23,8 +23,8 @@ function SocialButton() {
         return (
             <button type="button" onClick={handleClick}>
                 <Chat
-                    className="text-white drop-shadow-[0_0_0.3rem_#ffffff70] focus:bg-controlsSelected"
-                    width={32}
+                    className="rounded-sm text-primary drop-shadow-[0_0_0.3rem_#ffffff70] focus:bg-controlsSelected "
+                    width={42}
                     height="100%"
                 />
             </button>
@@ -60,7 +60,7 @@ export function NavigationBar({}): React.ReactElement {
 
     return (
         <>
-            <div className="relative flex h-16 w-full flex-row items-center justify-between bg-black/80 px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100 ">
+            <div className="relative flex h-16 w-full flex-row items-center justify-between bg-black/80 px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100">
                 <Link className="relative" href="/main">
                     <DoubleSharp
                         className="text-white drop-shadow-[0_0_0.3rem_#ffffff70]"
