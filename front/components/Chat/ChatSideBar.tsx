f0152c1f7d2c3ebc96a61b91aca5e78a4cbd5e71
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-04 17:46:04 +0900

    Replace room list mockups with code

--- front/components/Chat/ChatSideBar.tsx
+++ front/components/Chat/ChatSideBar.tsx
@@ -2,52 +2,70 @@ import React from "react";
 import SidebarIcon from "/public/sidebar.svg";
 import EditIcon from "/public/edit.svg";
 import ChatRoomBlock from "./ChatRoomBlock";
-import { TextField } from "../TextField";
+import { SearchBox } from "../TextField";
 
-const chatRoomsDummy = [
+type User = {
+    id: number;
+    name: string;
+};
+
+const users = [
+    { name: "chanhpar", id: 1 },
+    { name: "jisookim", id: 1 },
+    { name: "", id: 1 },
+    { name: "hdoo", id: 1 },
+];
+export type ChatRoomInfo = {
+    id: number;
+    members: User[];
+    title?: string;
+    latestMessage?: string;
+    numberOfUnreadMessages: number;
+};
+
+export const chatRoomsDummy: ChatRoomInfo[] = [
     {
         id: 1,
-        member: "hdoo",
-        tag: "#000011",
+        members: users,
         latestMessage: "맛있는 돈까스가 먹고싶어요 난 등심이 좋더라..",
-        nonReadYet: true,
+        numberOfUnreadMessages: 0,
     },
     {
         id: 2,
-        member: "chanhpar",
-        tag: "#000012",
+        members: [users[0], users[1]],
+        title: "glglgkgk",
         latestMessage: "옹옹엉양ㄹ오라ㅣㅁㄴ오맂다넝로미어ㅏ로미단로이머니",
-        nonReadYetMsgNum: 10,
+        numberOfUnreadMessages: 10,
     },
     {
         id: 3,
-        member: "iyun",
-        tag: "#000013",
+        members: [users[0], users[1]],
+        title: "러브포엠",
         latestMessage: "I'm IU ,,>ㅅ<,,",
-        nonReadYetMsgNum: 120,
+        numberOfUnreadMessages: 120,
     },
     {
         id: 4,
-        member: "jkong",
-        tag: "#000014",
+        members: [users[0], users[1]],
+        title: "Not donkikong",
         latestMessage: "I'm Jkong!",
-        nonReadYetMsgNum: 3,
+        numberOfUnreadMessages: 3,
     },
     {
         id: 5,
-        member: "jisookim",
-        tag: "#000015",
+        members: [users[0], users[1]],
+        title: "not Minsu",
         latestMessage: "Hi I'm jisoo",
-        nonReadYetMsgNum: 1029,
+        numberOfUnreadMessages: 1029,
     },
 ];
 
 export default function ChatSideBar() {
     return (
         <div>
-            <div className="gradient-border float-left flex h-full shrink-0 flex-col items-start gap-2 rounded-[0px_28px_28px_0px] bg-black/30 p-4 backdrop-blur-[50px] before:rounded-[28px] before:p-px before:content-['']">
+            <div className="gradient-border float-left flex h-full w-[310px] shrink-0 flex-col items-start gap-2 rounded-[0px_28px_28px_0px] bg-black/30 p-4 backdrop-blur-[50px] before:rounded-[28px] before:p-px before:content-['']">
                 <div className="flex h-16 shrink-0 items-center justify-between self-stretch px-2 py-4">
-                    <div className="flex w-fit items-center gap-2 self-stretch rounded-md">
+                    <div className="flex items-center gap-2 self-stretch rounded-md">
                         <EditIcon
                             className="text-gray-50"
                             width={22}
@@ -65,7 +83,13 @@ export default function ChatSideBar() {
                 </div>
 
                 {/* searchBar */}
-                <TextField />
+                <SearchBox />
+
+                <div>
+                    {chatRoomsDummy.map((room) => (
+                        <ChatRoomBlock key={room.id} chatRoom={room} />
+                    ))}
+                </div>
             </div>
         </div>
     );
