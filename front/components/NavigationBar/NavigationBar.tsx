04cca727996205b990a4ec473cad5c88afe22a8d
El <72256725+ElApostles@users.noreply.github.com>
2023-08-05 18:16:47 +0900

    Fix initial state of ChatModal

--- front/components/NavigationBar/NavigationBar.tsx
+++ front/components/NavigationBar/NavigationBar.tsx
@@ -14,7 +14,7 @@ import Chat from "/public/chat.svg";
 import Chatfilled from "/public/chatfilled.svg";
 
 function SocialButton() {
-    const [showModal, setShowModal] = useState(true);
+    const [showModal, setShowModal] = useState(false);
     const handleClick = () => setShowModal(!showModal);
 
     const mainNode = document.querySelector("main");
