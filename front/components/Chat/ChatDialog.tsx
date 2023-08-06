1534fcb64e523fcf18410581754307e041b37d7d
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-07 01:50:24 +0900

    Fit to Figma Layout

--- front/components/Chat/ChatDialog.tsx
+++ front/components/Chat/ChatDialog.tsx
@@ -10,12 +10,19 @@ function MessageInputArea() {
     return (
         <>
             <textarea
+                id="prompt-textarea"
+                tabIndex={0}
+                data-id="root"
                 rows={1}
-                className="h-fit max-h-20 w-full resize-none justify-center bg-transparent text-[15px] outline-none"
                 placeholder="Send a message"
-            />
+                className="h-6 max-h-20 w-full resize-none bg-transparent font-sans text-[15px] text-base font-light text-white/80 outline-none"
+            ></textarea>
             <button type="button" onClick={handleClick}>
-                <SendIcon className="text-gray-400" width={20} height="100%" />
+                <SendIcon
+                    className="text-gray-300/50"
+                    width={20}
+                    height="100%"
+                />
             </button>
         </>
     );
@@ -30,14 +37,14 @@ export function ChatDialog({
 }) {
     return (
         <div
-            className={`${width} flex min-h-[200px] flex-[1_0_0%] flex-col items-start justify-end gap-4 self-stretch`}
+            className={`${width} flex min-h-[200px] flex-auto flex-col items-start justify-end gap-4 self-stretch`}
         >
             <div
-                className={`${rounded} flex h-full w-full flex-[1_0_0%] flex-col items-center justify-end bg-black/30 p-4`}
+                className={`${rounded} flex h-full w-full flex-col items-center justify-end bg-black/30 p-4`}
             >
                 {/* TODO: add 말풍선 */}
                 <div className="flex flex-col items-center justify-center self-stretch">
-                    <div className="flex w-full max-w-[640px] flex-[1_0_0%] items-center rounded-xl bg-black/30 px-5 py-0">
+                    <div className="flex w-full max-w-[640px] flex-[1_0_0%] items-center rounded-xl bg-black/30 p-4">
                         <MessageInputArea />
                     </div>
                 </div>
