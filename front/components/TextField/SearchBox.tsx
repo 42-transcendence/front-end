5073ca9a9bbb83ec2ffa574143812fe89f3c92af
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-07 02:52:12 +0900

    remove SearchBox func, Change font

--- front/components/TextField/SearchBox.tsx
+++ front/components/TextField/SearchBox.tsx
@@ -13,11 +13,11 @@ export function TextField({
 }) {
     // TODO: reconsider children position or purpose?
     return (
-        <div className="shadow-3xl relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30 px-2 py-[5px] ">
+        <div className="shadow-3xl relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30 px-2 py-1">
             <div className="relative flex h-fit shrink-0 items-center justify-start gap-2 self-stretch rounded-xl px-2 py-0 ">
                 <div className="flex h-fit flex-col justify-start gap-2 overflow-hidden text-ellipsis font-sans text-sm font-light not-italic leading-[22px] text-gray-100/90">
                     <input
-                        className="border-[none] bg-transparent placeholder-gray-200/80 outline-none"
+                        className="h-6 resize-none border-[none] bg-transparent text-sm placeholder-gray-200/80 outline-none"
                         type="text"
                         placeholder={placeholder}
                         value={value}
@@ -26,16 +26,6 @@ export function TextField({
                 </div>
             </div>
             {children}
-        </div>
-    );
-}
-
-export function SearchBox() {
-    return (
-        <div className="shadow-3xl relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30 px-2 py-[5px]">
-            <div className="relative flex h-fit shrink-0 items-center justify-start gap-2 self-stretch rounded-xl px-2 py-0 ">
-                {/* <Icon className="float-left" type="search" size={30} /> */}
-            </div>
         </div>
     );
 }
