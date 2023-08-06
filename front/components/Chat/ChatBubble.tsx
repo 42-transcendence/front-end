b5fb5c5557d3b0dacd1bcdcca12f85b54bd724c9
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-07 01:51:04 +0900

    Add bubbletail svg, ChatBubble component

---
+++ front/components/Chat/ChatBubble.tsx
@@ -0,0 +1,13 @@
+import ChatBubbleTail from "/public/chat_bubble_tail.svg";
+
+export function ChatBubble() {
+    return (
+        <div>
+            <ChatBubbleTail
+                width="23.5"
+                height="12.71"
+                className="text-primary"
+            />
+        </div>
+    );
+}
