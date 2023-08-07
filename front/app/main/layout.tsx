56c1ffd01bd7df68b2a0c8a2637c4ebd62665f7f
chanhpar <chanhpar@student.42seoul.kr>
2023-08-07 21:10:42 +0900

    layout.tsx에서 getItem으로 access_token 값 유무 확인해서 로그인 체크 (검증은 안함)
    404 페이지 안뜨게 일단 임시로 /main/@login/default.tsx 파일 만듦

--- front/app/main/layout.tsx
+++ front/app/main/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 
 export default function MainLayout({
     login,
@@ -10,8 +10,12 @@ export default function MainLayout({
     main: React.ReactNode;
 }) {
     //TODO: 기존에 이미 인증된 상태(스토리지에 토큰이 있는 상태)라면 백으로 한번 보내서 검증시켜봐야 함.
-    const [loggedin, setLoggedin] = useState<boolean>(true);
+    const [loggedin, setLoggedin] = useState<boolean>(false);
+
     useEffect(() => {
+        setLoggedin(window.localStorage.getItem("access_token") !== null)
+        // console.log("token:", window.localStorage.getItem("access_token"));
+
         const receiveMessage = (event: StorageEvent) => {
             if (event.key === "access_token") {
                 //TODO: 토큰의 유효성 검증? 위의 로직과 겹칠 가능성이 있으므로 함수로 분리하는게 나을 수도
@@ -24,7 +28,7 @@ export default function MainLayout({
         return () => {
             window.removeEventListener("storage", receiveMessage);
         };
-    }, []);
+    }, [loggedin]);
 
     return <>{loggedin ? main : login}</>;
 }
