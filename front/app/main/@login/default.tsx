016321c9cf374bcd29c88355e7f16d87f17bda8a
chanhpar <chanhpar@student.42seoul.kr>
2023-08-07 21:10:42 +0900

    layout.tsx에서 getItem으로 access_token 값 유무 확인해서 로그인 체크 (검증은 안함)
    404 페이지 안뜨게 일단 임시로 /main/@login/default.tsx 파일 만듦

---
+++ front/app/main/@login/default.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Link from "next/link";
+
+export default function DefaultPage() {
+    return (
+        <main>
+            <p>default page</p>
+            <Link href="/main">go to main</Link>
+        </main>
+    );
+}
