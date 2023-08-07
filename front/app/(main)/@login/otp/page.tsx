922b042f3e3edf34e044f156efb44bfe5abd2343
chanhpar <chanhpar@student.42seoul.kr>
2023-08-07 13:30:33 +0900

    TODO: fix last digit input

--- front/app/(main)/@login/otp/page.tsx
+++ front/app/(main)/@login/otp/page.tsx
@@ -1,91 +1,121 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import React from "react";
+import { useState, useRef, } from "react";
 
-const updateInputConfig = (element: HTMLInputElement, disabledStatus: boolean) => {
-    element.disabled = disabledStatus;
-    if (!disabledStatus) {
-        element.focus();
-    } else {
-        element.blur();
-    }
-};
+function DigitBlock({
+    prev,
+    curr,
+    next,
+    value,
+    setValue,
+    onSubmit,
+}: {
+    prev: React.RefObject<HTMLInputElement> | null,
+    curr: React.RefObject<HTMLInputElement>,
+    next: React.RefObject<HTMLInputElement> | null,
+    value: string,
+    setValue: (value: string) => void,
+    onSubmit?: () => void,
+}) {
 
-function VerificationCodeBlock({ length, inputCount, setInputCount, }) {
-    const [value, setValue] = useState("");
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const currentValue = value;
 
-    const childEventListener = (e) => {
-        const replacedValue = e.target.value.replace(/[^0-9]/g, "").charAt(0);
+        const replacedValue = e.target.value.replace(/[^0-9]*/g, "")
+        setValue(replacedValue);
 
-        if (replacedValue.length === 1) {
-            updateInputConfig(e.target, true);
-            if (inputCount < length && e.key !== "Backspace") {
-                if (inputCount < length - 1) {
-                    updateInputConfig(e.target.nextElementSibling, false);
-                }
+        if (replacedValue !== "") {
+            // e.target.blur();
+            e.target.disabled = true;
+            const nextNode = next?.current;
+            if (nextNode) {
+                nextNode.disabled = false;
+                nextNode.focus();
             }
-            setInputCount(inputCount + 1);
-        } else if (replacedValue.length === 0 && e.key === "Backspace") {
-            if (inputCount === 0) {
-                updateInputConfig(e.target, false);
-                return false;
+            if (onSubmit) {
+                onSubmit();
             }
-            updateInputConfig(e.target, true);
-            e.target.previousElementSibling.value = "";
-            updateInputConfig(e.target.previousElementSibling, false);
-            setInputCount(inputCount - 1);
+        } else if (currentValue !== "") {
+            // e.target.blur();
+        }
+    };
+
+    const handleBackspace = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== "Backspace")
+            return;
+        const target = e.target as HTMLInputElement;
+        if (target.value === "" && prev !== null) {
+            target.disabled = true;
+            const prevNode = prev?.current;
+            if (prevNode) {
+                prevNode.disabled = false;
+                prevNode.focus();
+            }
         }
     }
 
+    // const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    //     e.target.select();
+    // }
+
+    // const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    // e.target.disabled = true;
+    // const nextNode = next.current;
+    // if (nextNode) {
+    //     nextNode.disabled = false;
+    //     nextNode.focus();
+    // }
+    // }
+
     return (
         <input
-            onKeyUp={childEventListener}
-            type="text"
+            className="ultra-dark flex h-12 w-12 rounded text-center opacity-80"
             inputMode="numeric"
+            type="text"
+            ref={curr}
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleBackspace}
+            // onFocus={handleFocus}
+            // onBlur={handleBlur}
             maxLength={1}
-            autoComplete="one-time-code"
-            disabled
-            // value={value}
-            className="ultra-dark flex appearance-none h-12 w-12 items-center rounded opacity-80"
+            disabled={prev !== null}
         />
     )
 }
 
-function VerificationCodeBlocks({ length, }: { length: number, }) {
-
-    const [inputCount, setInputCount] = useState(0);
-    const parentRef = useRef<HTMLElement>(null);
-
-    useEffect(() => {
-        const windowEventListener = (e: any) => { //TODO keyup event type
-            // TODO: for keyup event, is inputCount updated before this line?
-            if (inputCount >= length) {
-                alert("finalValue"); // TODO: submit value, validate
-                if (e.key === "Backspace") {
-                    const parentElement = parentRef.current;
-                    const lastChild = parentElement?.lastElementChild;
-                    if (lastChild) {
-                        updateInputConfig(lastChild, false);
-                        lastChild.value = "";
-                        setInputCount(inputCount - 1);
-                    }
-                }
-            }
-        }
-
-        window.addEventListener("keyup", windowEventListener);
-        return () => {
-            window.removeEventListener("keyup", windowEventListener);
-        }
-    }, [inputCount, length]);
-
-    return (
-        <div ref={parentRef} className="flex items-start gap-2 pt-14">
-            <VerificationCodeBlock length={length} inputCount={inputCount} />
-        </div>
-    );
-}
-
 export default function LoginPage() {
+    const [values, setValues] = useState(["", "", "", "", "", ""]);
+    const [result, setResult] = useState("");
+
+    const ref1 = useRef<HTMLInputElement>(null);
+    const ref2 = useRef<HTMLInputElement>(null);
+    const ref3 = useRef<HTMLInputElement>(null);
+    const ref4 = useRef<HTMLInputElement>(null);
+    const ref5 = useRef<HTMLInputElement>(null);
+    const ref6 = useRef<HTMLInputElement>(null);
+
+    const validateOTP = (otpValue: string) => {
+        const resultValue = Number(otpValue);
+        return (Number.isInteger(resultValue) && resultValue % 5 === 0)
+    }
+
+    const setValuesAt = (index: number) => (newValue: string) => {
+        setValues(values.map((x, i) => i === index ? newValue : x));
+    }
+
+    const handleSubmit = () => {
+        setResult(values.join(""));
+        if (validateOTP(result))
+            alert("correct");
+        setValues(["", "", "", "", "", ""]);
+        const first = ref1.current;
+        if (first) {
+            first.disabled = false;
+            first.focus();
+        }
+    }
+
     return (
         <main>
             <div className="flex items-center justify-center">
@@ -99,7 +129,15 @@ export default function LoginPage() {
                     </div>
                     {/* verify code block*/}
                     {/* TODO: 나중에 컴포넌트로 빼야 함! 근데 생각해보니.. 굳이..? 6개밖에 안쓸텐데... 흠..*/}
-                    <VerificationCodeBlocks length={6} />
+                    <div className="flex items-start gap-2 pt-14">
+                        <DigitBlock prev={null} curr={ref1} next={ref2} value={values[0]} setValue={setValuesAt(0)} />
+                        <DigitBlock prev={ref1} curr={ref2} next={ref3} value={values[1]} setValue={setValuesAt(1)} />
+                        <DigitBlock prev={ref2} curr={ref3} next={ref4} value={values[2]} setValue={setValuesAt(2)} />
+                        <DigitBlock prev={ref3} curr={ref4} next={ref5} value={values[3]} setValue={setValuesAt(3)} />
+                        <DigitBlock prev={ref4} curr={ref5} next={ref6} value={values[4]} setValue={setValuesAt(4)} />
+                        <DigitBlock prev={ref5} curr={ref6} next={null} value={values[5]} setValue={setValuesAt(5)} onSubmit={handleSubmit} />
+                    </div>
+                    <p>{result}</p>
                 </div>
             </div>
         </main>
