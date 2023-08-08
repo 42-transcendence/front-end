0ae832bd1cc4cf15a9fcbd7931b5f9c7534e84d0
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-08 22:59:02 +0900

    Add Awesome background

--- front/app/main/@main/layout.tsx
+++ front/app/main/@main/layout.tsx
@@ -4,7 +4,7 @@ import { NavigationBar } from "../../../components/NavigationBar";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <div className="flex flex-col self-stretch">
+            <div className="flex h-screen flex-col self-stretch">
                 <NavigationBar />
                 {children}
             </div>
