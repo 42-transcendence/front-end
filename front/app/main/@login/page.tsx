861eb570237dca1afeb4ff10e1b81c55b32b9276
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-09 15:43:01 +0900

    Remove dark theme from login page

--- front/app/main/@login/page.tsx
+++ front/app/main/@login/page.tsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
             <div className="relative flex h-screen w-screen flex-col items-center justify-center ">
                 <div className="relative flex h-full items-end justify-center">
                     <DoubleSharp
-                        className="relative h-fit w-fit animate-spin-slow text-black drop-shadow-[0_0_3rem_#00000050] delay-300 dark:text-white dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                        className="relative h-fit w-fit animate-spin-slow delay-300 text-white drop-shadow-[0_0_0.3rem_#ffffff70]"
                         width={130}
                         height="100%"
                     />
--- front/components/Button/LoginButton.tsx
+++ front/components/Button/LoginButton.tsx
@@ -12,11 +12,11 @@ export function LoginButton({
         <button
             type="button"
             onClick={onClick}
-            className="gradient-border relative flex h-12 w-48 flex-col justify-center rounded bg-windowGlass/30 px-4 py-[1px] backdrop-blur-[20px] backdrop-brightness-100 before:absolute before:inset-0 before:rounded before:p-px before:content-[''] hover:bg-controlsSelected dark:bg-black/30"
+            className="gradient-border relative flex h-12 w-48 flex-col justify-center rounded bg-black/30 px-4 py-[1px] backdrop-blur-[20px] backdrop-brightness-100 before:absolute before:inset-0 before:rounded before:p-px before:content-[''] hover:bg-controlsSelected"
         >
             <div className="flex items-center gap-2 py-2.5">
                 {icon}
-                <p className="font-sans text-sm font-medium text-neutral-800 dark:text-neutral-200">
+                <p className="font-sans text-sm font-medium text-neutral-200">
                     {children}
                 </p>
             </div>
