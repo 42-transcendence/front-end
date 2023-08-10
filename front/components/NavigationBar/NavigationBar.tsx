f9cc039a453d6d21b2227ec625e245a75c52e69e
hdoo <tyeirdoo06@gmail.com>
2023-08-11 03:47:34 +0900

    remove blur from global backblur

--- front/components/NavigationBar/NavigationBar.tsx
+++ front/components/NavigationBar/NavigationBar.tsx
@@ -22,7 +22,7 @@ export async function NavigationBar() {
     // console.log(account);
 
     return (
-        <div className="relative flex h-16 w-full flex-row items-center justify-between bg-primary/30 px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100">
+        <div className="relative flex h-12 w-full flex-row items-center justify-between bg-primary/30 px-5 py-2.5 backdrop-blur-[20px] backdrop-brightness-100 2xl:h-16">
             <Link className="relative" href="/main">
                 <DoubleSharp
                     className="text-white drop-shadow-[0_0_0.3rem_#ffffff70]"
