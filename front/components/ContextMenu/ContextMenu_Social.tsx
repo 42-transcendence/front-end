6e6a38ce2a6cc1e486fd564c2578b33605c71294
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-11 17:23:03 +0900

    Add red background to critical menus

--- front/components/ContextMenu/ContextMenu_Social.tsx
+++ front/components/ContextMenu/ContextMenu_Social.tsx
@@ -10,12 +10,20 @@ export function ContextMenu_Social({
 }: {
     profile: ProfileItemConfig;
 }) {
+    //TODO : friend add logic : check whether user is already friend or not.
+
     return (
         <>
             <ContextMenuItem name="태그 복사하기" className="text-base" />
             <ContextMenuItem name="친구 추가하기" className="text-base" />
-            <ContextMenuItem name="차단하기" className="text-base" />
-            <ContextMenuItem name="신고하기" className="text-base" />
+            <ContextMenuItem
+                name="차단하기"
+                className="text-base hover:bg-red-500/30"
+            />
+            <ContextMenuItem
+                name="신고하기"
+                className="text-base hover:bg-red-500/30"
+            />
         </>
     );
 }
