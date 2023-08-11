b016c7b5cf63e156795f91bc1f3479c86d47d329
jisookim <67938113+nimo-my@users.noreply.github.com>
2023-08-11 22:17:27 +0900

    로그인 후, 닉네임 입력창 @main/welcome 폴더안에 제작, 메인화면 새로 제작 #70

--- front/app/main/@main/CreateGame.tsx
+++ front/app/main/@main/CreateGame.tsx
@@ -1,41 +1,14 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 export function CreateGame() {
-    const router = useRouter();
-
-    // FIXME: 실제로 fetch 하면 시간 걸리니까 일단 편의상 만들어둔 dummy async 함수
-    const asyncRoute = async () => {
-        await new Promise((res) => setTimeout(res, 0));
-        router.push("/main/game/4242");
-    };
-
-    // TODO: 실제로는 백에서 채팅방 받아와야. fetch할때 방 정보, 모드 등 parameter로 붙여서 요청
-    // const handleClick = async () => {
-
-    //     const res = await fetch(
-    //         "https://www.random.org/integers/?num=1&min=1&max=1000&col=1&base=10&format=plain&rnd=new",
-    //         { next: { revalidate: 0 } },
-    //     );
-    //     if (!res.ok) {
-    //         throw new Error("fetch failure");
-    //     }
-    //     const newGameId = await res.json();
-
-    //     router.push(`/main/game/${newGameId}`);
-    // }
 
     return (
         <button
             type="button"
-            onClick={asyncRoute}
-            // href="/main/game/1234"
+            // onClick={asyncRoute}
             className="relative flex place-items-center"
         >
             <div className="gradient-border before:pointer-elvents-none group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[20px] backdrop-brightness-100 transition-colors before:absolute before:inset-0 before:rounded-[28px] before:p-px before:content-[''] hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
                 <h2 className={"text-sans  text-2xl font-medium "}>
-                    Go to Game
+                    Create Game
                 </h2>
             </div>
         </button>
--- front/app/main/@main/page.tsx
+++ front/app/main/@main/page.tsx
@@ -1,30 +1,50 @@
 import Link from "next/link";
 import { CreateGame } from "./CreateGame";
+import { QuickMatch } from "./QuickMatch";
+import { DoubleSharp } from "@/components/ImageLibrary";
+import { Game_Ghost3 } from "@/components/ImageLibrary";
+
+export function HelloWorld() {
+    return (
+        <div className="z-10 min-w-max">
+            <div className="flex flex-row gap-[25px] pt-[10px]">
+                <Game_Ghost3 width="80" height="80" color="#00FFD1" />
+                <div className="flex flex-col pb-[59px] text-center text-[32px] font-bold italic leading-[45px] text-[#00FFD1]">
+                    <div>HELLO, IT'S</div>
+                    <div>DOUBLE SHARP!</div>
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <div className="pb-[46px] text-center text-[24px] font-bold not-italic leading-[45px] text-white">
+                    <div>안녕하세요! 당신을 위한 핑퐁 게임,</div>
+                    <div>[더블샵]에 오신 것을 환영합니다!</div>
+                </div>
+                <div className="text-center text-base font-bold not-italic leading-[28px] text-white">
+                    <p>새 게임 만들기 - Create Game</p>
+                    <p>빠른 대전 - Quick Match</p>
+                </div>
+                <div className="pt-[20px] text-center text-base font-normal italic leading-5 text-white text-white/30">
+                    @42-transendence
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default function Home() {
     return (
-        <>
-            <Link
-                href="/main/dropdown"
-                className="relative flex place-items-center"
-            >
-                <div className="gradient-border before:pointer-elvents-none group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[20px] backdrop-brightness-100 transition-colors before:absolute before:inset-0 before:rounded-[28px] before:p-px before:content-[''] hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
-                    <h2 className={"text-sans text-2xl font-medium "}>
-                        Dropdown menu
-                    </h2>
+        <div className="backblur h-full w-full bg-black/30 before:backdrop-blur-[3px] before:lg:backdrop-blur-[1px] flex items-center justify-center">
+        
+            <div className="flex flex-[1_0_0] flex-col items-center justify-center gap-2.5 self-stretch px-[700px] py-[147px] lg:flex-row">
+                <div className="flex flex-col items-start">
+                    <HelloWorld />
                 </div>
-            </Link>
-            <Link
-                href="/main/social"
-                className="relative flex place-items-center"
-            >
-                <div className="gradient-border before:pointer-elvents-none group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[20px] backdrop-brightness-100 transition-colors before:absolute before:inset-0 before:rounded-[28px] before:p-px before:content-[''] hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
-                    <h2 className={"text-sans  text-2xl font-medium "}>
-                        Social menu
-                    </h2>
+
+                <div className="flex h-[300px] flex-col items-center justify-center gap-[30px] px-[75px] py-[69px]">
+                    <QuickMatch />
+                    <CreateGame />
                 </div>
-            </Link>
-            <CreateGame />
-        </>
+            </div>
+        </div>
     );
 }
