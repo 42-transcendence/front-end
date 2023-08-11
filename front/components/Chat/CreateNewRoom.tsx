5e9387ad8ad968709e4235d415717eb13e5d9c42
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-11 22:30:27 +0900

    Cute animation to textfield

--- front/components/Chat/CreateNewRoom.tsx
+++ front/components/Chat/CreateNewRoom.tsx
@@ -1,34 +1,75 @@
-import { useEffect, useRef, useState } from "react";
-import LockIcon from "/public/lock.svg";
-import PasswordIcon from "/public/password.svg";
-import { TextField } from "../TextField";
+"use client";
 
-export function CreateNewRoom() {
+import { useState } from "react";
+import LockIcon from "/public/lock.svg";
+import { TextField } from "../TextField";
+import { IconKey, IconLock } from "../ImageLibrary";
+
+export function CreateNewRoom({ className }: { className: string }) {
     const [title, setTitle] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div className="backblur absolute left-0 top-24 flex h-full w-full flex-col gap-2 rounded-[28px_28px_0px_0px] bg-black/30 p-4 ">
-            <p className="pl-3 text-xs text-red-500/80">{title}</p>
-            <TextField
-                className="p-3"
-                value={title}
-                onChange={(event) => {
-                    setTitle(event.target.value);
-                }}
-            />
-            <p className="invisible pl-3 text-xs text-red-500/80 peer-invalid:visible">
-                Title too long
-            </p>
-            <div className="z-10 flex h-fit w-full flex-row justify-around rounded-xl bg-black/30 p-3">
-                <LockIcon
-                    width={96}
-                    height={96}
-                    className="rounded-xl bg-primary/80 p-4 text-gray-50/80"
+        <div
+            className={`${className} absolute right-0 top-24 z-50 flex h-full w-full flex-col gap-4 rounded-[28px_28px_0px_0px] bg-gray-800 p-4`}
+        >
+            <div>
+                <TextField
+                    type="search"
+                    className="relative bg-transparent p-3 transition-all focus-within:py-5"
+                    placeholder="Title..."
+                    value={title}
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                    }}
                 />
-                <PasswordIcon
-                    width={96}
-                    height={96}
-                    className="rounded-xl bg-primary/80 p-4 text-gray-50/80"
+                <hr className="border-gray-200/50" />
+            </div>
+
+            <div className="relative flex flex-row items-center gap-3">
+                <input
+                    type="checkbox"
+                    id="private"
+                    className="peer/private hidden"
                 />
+                <label
+                    htmlFor="private"
+                    className=" gap-2 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors peer-checked/private:bg-secondary peer-checked/private:text-gray-50/80"
+                >
+                    <IconLock width={24} height={24} className="" />
+                </label>
+                <p className="text-sm peer-checked/private:hidden">공개</p>
+                <p className="hidden text-sm peer-checked/private:block">
+                    비공개
+                </p>
+            </div>
+            <div className="relative flex w-full flex-row items-start gap-3">
+                <input
+                    type="checkbox"
+                    id="secret"
+                    className="peer/secret hidden"
+                />
+                <label
+                    htmlFor="secret"
+                    className=" gap-2 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors peer-checked/secret:bg-secondary peer-checked/secret:text-gray-50/80"
+                >
+                    <IconKey width={24} height={24} className="" />
+                </label>
+
+                <div className="absolute left-[4.25rem] top-4 items-end justify-center text-sm transition-all peer-checked/secret:absolute peer-checked/secret:left-[4.25rem] peer-checked/secret:top-0">
+                    비밀번호
+                </div>
+                <div className="relative hidden h-full flex-col items-start justify-end gap-1 text-sm peer-checked/secret:flex">
+                    <TextField
+                        type="password"
+                        placeholder="..."
+                        className="bg-black/30 p-4 placeholder-gray-500/30"
+                        value={password}
+                        onChange={(event) => {
+                            setPassword(event.target.value);
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
