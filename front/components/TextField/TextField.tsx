bd4a3e3ce45ea26e13736376565a1042bd0a8c17
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-12 17:42:49 +0900

    Change textfield to accept all attributes of input

--- front/components/TextField/TextField.tsx
+++ front/components/TextField/TextField.tsx
@@ -1,33 +1,23 @@
-import React, { HTMLInputTypeAttribute } from "react";
+import React, { InputHTMLAttributes } from "react";
 
-export function TextField({
-    icon,
-    value,
-    type,
-    placeholder,
-    onChange,
-    className,
-}: {
+type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     icon?: React.ReactNode | undefined;
-    value: string;
-    type: HTMLInputTypeAttribute;
-    placeholder?: string | undefined;
-    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
     className?: string | undefined;
-}) {
+};
+
+export function TextField<T extends TextFieldProps>(props: T) {
     // TODO: reconsider children position or purpose?
+    const { icon, className, ...rest } = props;
+
     return (
-        <form
-            className={`shadow-3xl group relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30 ${className}`}
+        <div
+            className={`${className} shadow-3xl group relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30`}
         >
             <input
                 className="peer h-6 w-full resize-none border-[none] bg-transparent font-sans text-sm font-normal placeholder-gray-200/30 outline-none"
-                type={type}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
+                {...rest}
             />
             {icon}
-        </form>
+        </div>
     );
 }
