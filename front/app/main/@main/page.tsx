3eeb5630058c2a10ad2921a135b33b41dad7b314
chanhpar <chanhpar@student.42seoul.kr>
2023-08-13 01:36:38 +0900

    LogoutButton.tsx에서 style 관련 태그 제거. 해당 파일에는 Button만 남기고 호출하는 쪽에서 style지정하도록 함

--- front/app/main/@main/page.tsx
+++ front/app/main/@main/page.tsx
@@ -40,7 +40,11 @@ export default function Home() {
                 <div className="flex h-[300px] flex-col items-center justify-center gap-[30px] px-[75px] py-[69px]">
                     <QuickMatchButton />
                     <CreateGameButton />
-                    <LogoutButton />
+                    <div className="gradient-border before:pointer-elvents-none group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[20px] backdrop-brightness-100 transition-colors before:absolute before:inset-0 before:rounded-[28px] before:p-px before:content-[''] hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
+                        <h2 className={"text-sans  text-2xl font-medium "}>
+                            <LogoutButton />
+                        </h2>
+                    </div>
                 </div>
             </div>
         </div>
