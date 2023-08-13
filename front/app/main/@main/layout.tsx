fcede0d6b111f17ae6dccb8b062751c9d2c75d49
jkong <jkong@student.42seoul.kr>
2023-08-13 20:28:57 +0900

    전체적으로 한번 다듬었습니다.
    UIFrame 대신 Card라는 이름을 사용하도록 했습니다.
    UIFrame이 default export 대신 named export를 사용하게 했습니다.
    Welcome2 페이지에서 사용한 의존성 배열을 linter가 알려주는대로 변경했습니다.
    게임 버튼을 통합했습니다. 추후 컴포넌트로 만들 수 있어보입니다.

--- front/app/main/@main/layout.tsx
+++ front/app/main/@main/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavigationBar } from "../../../components/NavigationBar";
+import { NavigationBar } from "@/components/NavigationBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-[100dvh] flex-shrink-0 flex-col">
             <NavigationBar />
-            <main className=" relative flex h-full flex-col items-center justify-center gap-1 justify-self-stretch overflow-auto">
+            <main className="relative flex h-full flex-col items-center justify-center gap-1 justify-self-stretch overflow-auto">
                 {children}
             </main>
         </div>
