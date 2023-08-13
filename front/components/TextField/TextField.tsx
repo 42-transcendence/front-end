5f7f124f4e680b2f0f15ee3e25b9de65aac82f7d
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-13 16:29:31 +0900

    Text field cleanup.

--- front/components/TextField/TextField.tsx
+++ front/components/TextField/TextField.tsx
@@ -5,19 +5,19 @@ type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     className?: string | undefined;
 };
 
-export function TextField<T extends TextFieldProps>(props: T) {
+export function TextField(props: TextFieldProps) {
     // TODO: reconsider children position or purpose?
     const { icon, className, ...rest } = props;
 
     return (
-        <div
-            className={`${className} shadow-3xl group relative flex h-8 shrink-0 items-center justify-between gap-2 self-stretch rounded-xl bg-black/30`}
+        <fieldset
+            className={`${className} shadow-3xl group relative flex h-8 shrink-0 items-center justify-center gap-2 self-stretch rounded-xl bg-black/30`}
         >
             <input
-                className="peer h-6 w-full resize-none border-[none] bg-transparent font-sans text-sm font-normal placeholder-gray-200/30 outline-none"
+                className="peer h-6 w-full resize-none border-[none] bg-transparent font-sans font-normal placeholder-gray-200/30 outline-none"
                 {...rest}
             />
             {icon}
-        </div>
+        </fieldset>
     );
 }
