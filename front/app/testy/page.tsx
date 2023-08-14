a64308e83869aa3ebdf7333472fe57e4125b815c
chanhpar <chanhpar@student.42seoul.kr>
2023-08-14 17:44:11 +0900

    Game결과페이지의 component들 이름변경, 한 파일로 합치기 default export 를 named export로 수정
    ClikNotificationCard -> PlayAgainCard
    AchivementCard -> TrophyCard
    GameAchievementCard -> NewAchievementCard

--- front/app/testy/page.tsx
+++ front/app/testy/page.tsx
@@ -1,21 +1,20 @@
-import AchivementCard from "@/components/Game/AchivementCard";
-import GameNavBar from "@/components/Game/GameNavBar";
-import NotificationCard from "@/components/Game/NotificationCard";
-import GameUserFrame from "@/components/Game/GameUserFrame";
-import GameAchievementCard, {
-    ClikNotificationCard,
-} from "@/components/Game/NotificationCard";
+import { GameUserFrame } from "@/components/Game/GameUserFrame";
+import {
+    PlayAgainCard,
+    NewAchievementCard,
+    TrophyCard,
+} from "@/components/Game/AfterGameCards";
 
 export default function Main() {
     return (
         <div>
             <GameUserFrame />
             <div className=" m-20"></div>
-            <AchivementCard />
+            <TrophyCard />
             <div className=" m-20"></div>
-            <ClikNotificationCard />
+            <PlayAgainCard />
             <div className=" m-20"></div>
-            <GameAchievementCard />
+            <NewAchievementCard />
         </div>
     );
 }
