4ef32dd022ef7133a5a686f35e6749e13cccbaee
chanhpar <chanhpar@student.42seoul.kr>
2023-08-15 22:55:15 +0900

    /main/testy의page.tsx 컴포넌트들을 좀 잘게 쪼갰습니다. 이름은 일단 임시로지어놨습니다.
    더불어 game점수에 따라 win /lose 메세지 출력 등 아주 약간 조금의 로직분리를 했습니다

--- front/app/testy/page.tsx
+++ front/app/testy/page.tsx
@@ -8,8 +8,10 @@ import Image from "next/image";
 import {
     Game_You,
     Game_Win,
+    Game_Lose,
     IconExclamationMark,
 } from "@/components/ImageLibrary";
+import { TimeoutWrapper } from "./TimeoutWrapper";
 
 // TODO: 시간이 지나면 사라지는 기능 추가
 function PositionTopLeft() {
@@ -29,42 +31,73 @@ function PositionTopRight() {
     );
 }
 
-function Score() {
+function LeftFlag() {
+    return (
+        <Image
+            className=""
+            src={"/left-flag.png"}
+            alt=""
+            width="300"
+            height="200"
+        />
+    );
+}
+
+function RightFlag() {
+    return (
+        <Image
+            className=""
+            src={"/right-flag.png"}
+            alt=""
+            width="300"
+            height="200"
+        />
+    );
+}
+
+// TODO: fix logic: game score result, achievement, user info.
+// decide win or lose
+function GameResultBanner() {
+    const scoreResult = { team1: 3, team2: 21 }
+    const scoreStyle = {
+        team1: scoreResult.team1 > scoreResult.team2 ? "text-[#FFD600]" : "",
+        team2: scoreResult.team1 < scoreResult.team2 ? "text-[#FFD600]" : "", // TODO: win / lose / tie ?
+    }
+
+    function WinOrLoseMessage() {
+        const winOrLose = true;
+        return (
+            <div className="flex flex-row gap-6">
+                <Game_You width="120" height="120" />
+                {winOrLose ? <Game_Win width="120" height="120" /> : <Game_Lose width="120" height="120" />}
+                <IconExclamationMark
+                    width="60"
+                    height="60"
+                    className="mt-7"
+                />
+            </div>
+        );
+    }
+
+    function GameResultScore() {
+        return (
+            <div className="space-x-3 text-5xl">
+                <span className={scoreStyle.team1}>{scoreResult.team1}</span>
+                <span>:</span>
+                <span className={scoreStyle.team2}>{scoreResult.team2}</span>
+            </div>
+        );
+    }
+
     return (
         <>
-            <PositionTopLeft /> {/* NewAchievementCard */}
-            <PositionTopRight /> {/* PlayAgainCard */}
             <div className="flex items-center justify-center gap-6 px-[15rem] pb-24 pt-32">
-                <Image
-                    className=""
-                    src={"/left-flag.png"}
-                    alt="left-flag"
-                    width="300"
-                    height="200"
-                />
+                <LeftFlag />
                 <div className="flex flex-col items-center justify-center px-12 py-5">
-                    <div className="flex flex-row gap-6">
-                        <Game_You width="120" height="120" />
-                        <Game_Win width="120" height="120" />
-                        <IconExclamationMark
-                            width="60"
-                            height="60"
-                            className="mt-7"
-                        />
-                    </div>
-                    <div className="space-x-3 text-5xl">
-                        <span className="text-[#FFD600]">3</span>
-                        <span className="">:</span>
-                        <span className="">2</span>
-                    </div>
+                    <WinOrLoseMessage />
+                    <GameResultScore />
                 </div>
-                <Image
-                    className=""
-                    src={"/right-flag.png"}
-                    alt="right-flag"
-                    width="300"
-                    height="200"
-                />
+                <RightFlag />
             </div>
         </>
     );
@@ -74,7 +107,11 @@ export default function Main() {
     return (
         <div>
             <div className="flex h-screen flex-col items-center backdrop-blur-lg">
-                <Score />
+                <TimeoutWrapper milliseconds={5000}>
+                    <PositionTopLeft /> {/* NewAchievementCard */}
+                </TimeoutWrapper>
+                <PositionTopRight /> {/* PlayAgainCard */}
+                <GameResultBanner />
                 <div className="flex flex-row items-center gap-5">
                     <GameUserFrame />
                     <GameUserFrame />
