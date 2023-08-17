c2034efb1251590ed8b4764edf6820cc9604ea4c
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-17 21:21:03 +0900

    Add primary darker to tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -13,16 +13,17 @@ module.exports = {
                 priVar: "rgb(var(--color-primary-varient), <alpha-value>)",
                 secondary: "rgb(var(--color-secondary), <alpha-value>)",
                 seconVar: "rgb(var(--color-secondary-varient), <alpha-value>)",
-                seconVar2:
-                    "rgb(var(--color-secondary-varient2), <alpha-value>)",
+                seconDarker:
+                    "rgb(var(--color-secondary-darker), <alpha-value>)",
+                priDarker: "rgb(var(--color-primary-darker), <alpha-value>)",
                 windowGlass: "rgb(var(--windows-glass), <alpha-value>)",
                 controlsHover: "rgb(var(--controls-hover), <alpha-value>)",
                 controlsSelected:
                     "rgb(var(--controls-selected), <alpha-value>)",
                 ultraDark: "rgb(197 197 197 / <alpha-value>)",
-                iconTrinary: "color-mix(in srgb, #5e5e5e 64%, #ffffff 22%)",
-                iconSecondary: "rgb(105 105 105 / <alpha-value>)",
                 iconPrimary: "rgb(230 230 230 / <alpha-value>)",
+                iconSecondary: "rgb(105 105 105 / <alpha-value>)",
+                iconTrinary: "color-mix(in srgb, #5e5e5e 64%, #ffffff 22%)",
                 darker: "color-mix(in srgb, #000000 8%, #d6d6d6 45%)",
             },
 
