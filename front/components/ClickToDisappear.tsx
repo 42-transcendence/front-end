bd2ef4e360960bacc116006b35e2b15ad6608a7d
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-18 17:32:35 +0900

    Front page

--- front/components/ClickToDisappear.tsx
+++ front/components/ClickToDisappear.tsx
@@ -1,12 +1,13 @@
-"usee client";
+"use client";
 
 import { usePathname } from "next/navigation";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, InputHTMLAttributes } from "react";
+import { DoubleSharp } from "./ImageLibrary";
 
 function SplashScreen({
-    setIsMounted
+    setIsMounted,
 }: {
-    setIsMounted: React.Dispatch<React.SetStateAction<boolean>>
+    setIsMounted: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
     const divRef = useRef<HTMLDivElement>(null);
 
@@ -16,7 +17,7 @@ function SplashScreen({
         if (divElement === null) return;
 
         divElement.classList.add("opacity-0");
-    }
+    };
 
     const handleKeyDown = (event: KeyboardEvent) => {
         const divElement = divRef.current;
@@ -25,31 +26,27 @@ function SplashScreen({
         if (event.key === "Enter" || event.key === " ") {
             divElement.classList.add("opacity-0");
         }
-    }
+    };
 
     const handleTransition = () => {
         setIsMounted(false);
-    }
+    };
 
     useEffect(() => {
         console.log("hello");
-        window.addEventListener("keydown", handleKeyDown, /* { once: true } */); // TODO: check if neccessary
-        window.addEventListener("click", handleClick, /* { once: true } */);
+        window.addEventListener("keydown", handleKeyDown /* { once: true } */); // TODO: check if neccessary
+        window.addEventListener("click", handleClick /* { once: true } */);
 
         return () => {
             console.log("bye");
             window.removeEventListener("keydown", handleKeyDown);
             window.removeEventListener("click", handleClick);
-        }
+        };
     }, []);
 
     return (
-        <div
-            ref={divRef}
-            className="z-10 transition-opacity bg-windowGlass/30 duration-500 bg-green-600 absolute h-full w-full"
-            onTransitionEnd={handleTransition}
-        >
-            click to start / press Enter
+        <div ref={divRef} className="transition-opacity duration-500">
+            <FrontPage onTransitionEnd={handleTransition}></FrontPage>
         </div>
     );
 }
@@ -65,9 +62,34 @@ export function ClickToDisappear() {
 
     return (
         <>
-            {isRootPage && isMounted ?
+            {isRootPage && isMounted ? (
                 <SplashScreen setIsMounted={setIsMounted} />
-                : null}
+            ) : null}
         </>
+    );
+}
+
+function FrontPage(props: InputHTMLAttributes<HTMLDivElement>) {
+    const { ...rest } = props;
+
+    return (
+        <div
+            className="absolute z-10 flex h-full w-full flex-col items-center justify-center bg-main bg-cover"
+            {...rest}
+        >
+            <div className=" flex h-full flex-col justify-center">
+                <DoubleSharp
+                    width={200}
+                    height={200}
+                    className="drop-shadow-[0_0_1rem_#ffffff70]"
+                />
+            </div>
+
+            <div className="h-1/5">
+                <div className="flex  h-12 w-56 items-center justify-center rounded-[28px_28px_0_0] bg-white font-sans text-3xl font-extrabold text-black">
+                    Game Start
+                </div>
+            </div>
+        </div>
     );
 }
