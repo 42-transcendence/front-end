8414e05140e7872433182c0d62f2b1aafda3bf72
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-18 23:14:31 +0900

    drop down menus of side bar

--- front/components/Chat/ChatMemberMenu.tsx
+++ front/components/Chat/ChatMemberMenu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MenuItem } from "./MenuItem";
 
-export function ChatRoomMenu({
+export function ChatMemberMenu({
     className,
     isAdmin,
 }: {
@@ -12,20 +12,16 @@ export function ChatRoomMenu({
         <div
             className={`${className} flex-col items-center text-base font-bold text-gray-100/80`}
         >
-            <MenuItem className="active:bg-secondary/80">알림 설정</MenuItem>
             {isAdmin && (
                 <MenuItem className="active:bg-secondary/80">
-                    소유권 양도
+                    정지 유저 관리
                 </MenuItem>
             )}
             {isAdmin && (
-                <MenuItem className="hover:bg-red-500/80 active:bg-secondary/80">
-                    채팅방 삭제
+                <MenuItem className="active:bg-secondary/80">
+                    채팅금지 유저 관리
                 </MenuItem>
             )}
-            <MenuItem className="hover:bg-red-500/80 active:bg-secondary/80">
-                방 나가기
-            </MenuItem>
         </div>
     );
 }
