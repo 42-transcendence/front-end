6ea0eb2cd4ad9d61a5e6ea6478dc85b3eb6446c5
chanhpar <chanhpar@student.42seoul.kr>
2023-08-19 11:22:36 +0900

    refactor: minor change in SocialButton

--- front/components/NavigationBar/SocialButton.tsx
+++ front/components/NavigationBar/SocialButton.tsx
@@ -8,16 +8,19 @@ import { IconChatOutlined, IconChatFilled } from "@/components/ImageLibrary";
 export function SocialButton() {
     const pathname = usePathname();
     const [prevPathname, setPrevPathname] = useState(pathname);
+    const isOnChatPage = pathname === "/chat";
 
-    const ChatIcon = pathname === "/chat" ? IconChatFilled : IconChatOutlined;
+    const ChatIcon = isOnChatPage ? IconChatFilled : IconChatOutlined;
 
     const handleClick = () => {
         setPrevPathname(pathname);
     };
 
+    // TODO: href 이동으로 인한 pathname 업데이트와 handleClick 내부
+    // setPrevPathname 사이의 실행순서가 보장되는가?
     return (
         <Link
-            href={pathname === "/chat" ? prevPathname : "/chat"}
+            href={isOnChatPage ? prevPathname : "/chat"}
             onClick={handleClick}
         >
             <ChatIcon className="h-12 w-12 rounded-lg p-2 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14" />
