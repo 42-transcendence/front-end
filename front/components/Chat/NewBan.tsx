c5ac2f0aaf251cdc24ef6d0cf17e5e03e1afa918
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-21 03:40:21 +0900

    Change to inline arrow function

--- front/components/Chat/NewBan.tsx
+++ front/components/Chat/NewBan.tsx
@@ -1,12 +1,23 @@
+import { ProfileItemBase } from "../ProfileItem/ProfileItembase";
+
+const configMockup = {
+    id: 123,
+    uuid: "123",
+    tag: "#123",
+    name: "hdoo",
+    statusMessage: "nothion",
+};
+
 export function AccessBan({ uuid }: { uuid: string }) {
     //TODO; fetch from uuid
     void uuid;
 
     return (
-        <div className="flex flex-col items-start justify-center">
-            <div className="h-8 w-full items-center justify-center text-base">
-                <h1>타임아웃</h1>
-            </div>
+        <div className="flex h-full w-full flex-col justify-start">
+            <p className="w-fit text-sm font-bold text-gray-100">
+                다음 유저를 현재 채팅방에서 내보냅니다.
+            </p>
+            <ProfileItemBase className="" />
         </div>
     );
 }
