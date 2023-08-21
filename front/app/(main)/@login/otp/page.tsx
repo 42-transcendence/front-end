eba80ab1939d56097fafb71eab9f28914fb8d002
chanhpar <chanhpar@student.42seoul.kr>
2023-08-21 17:15:05 +0900

    remove result state. validateOTP inside useEffect. remove handleSubmit function

--- front/app/(main)/@login/otp/page.tsx
+++ front/app/(main)/@login/otp/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { useState, useRef, } from "react";
+
+import { useState, useRef, useCallback, useEffect } from "react";
 
 function DigitBlock({
     prev,
@@ -8,35 +8,30 @@ function DigitBlock({
     next,
     value,
     setValue,
-    onSubmit,
+    // onSubmit,
 }: {
     prev: React.RefObject<HTMLInputElement> | null,
     curr: React.RefObject<HTMLInputElement>,
     next: React.RefObject<HTMLInputElement> | null,
     value: string,
     setValue: (value: string) => void,
-    onSubmit?: () => void,
+    // onSubmit?: () => void,
 }) {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const currentValue = value;
-
         const replacedValue = e.target.value.replace(/[^0-9]*/g, "")
         setValue(replacedValue);
 
         if (replacedValue !== "") {
-            // e.target.blur();
             e.target.disabled = true;
             const nextNode = next?.current;
             if (nextNode) {
                 nextNode.disabled = false;
                 nextNode.focus();
             }
-            if (onSubmit) {
-                onSubmit();
-            }
-        } else if (currentValue !== "") {
-            // e.target.blur();
+            // if (onSubmit) {
+            //     onSubmit();
+            // }
         }
     };
 
@@ -54,22 +49,10 @@ function DigitBlock({
         }
     }
 
-    // const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    //     e.target.select();
-    // }
-
-    // const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    // e.target.disabled = true;
-    // const nextNode = next.current;
-    // if (nextNode) {
-    //     nextNode.disabled = false;
-    //     nextNode.focus();
-    // }
-    // }
-
     return (
         <input
             className="ultra-dark flex h-12 w-12 rounded text-center opacity-80"
+            autoComplete="one-time-code"
             inputMode="numeric"
             type="text"
             ref={curr}
@@ -84,9 +67,15 @@ function DigitBlock({
     )
 }
 
+const sleep = async (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const validateOTP = (otpValue: string) => {
+    const resultValue = Number(otpValue);
+    return (Number.isInteger(resultValue) && resultValue > 0 && resultValue % 5 === 0)
+}
+
 export default function LoginPage() {
     const [values, setValues] = useState(["", "", "", "", "", ""]);
-    const [result, setResult] = useState("");
 
     const ref1 = useRef<HTMLInputElement>(null);
     const ref2 = useRef<HTMLInputElement>(null);
@@ -95,26 +84,31 @@ export default function LoginPage() {
     const ref5 = useRef<HTMLInputElement>(null);
     const ref6 = useRef<HTMLInputElement>(null);
 
-    const validateOTP = (otpValue: string) => {
-        const resultValue = Number(otpValue);
-        return (Number.isInteger(resultValue) && resultValue % 5 === 0)
-    }
-
     const setValuesAt = (index: number) => (newValue: string) => {
         setValues(values.map((x, i) => i === index ? newValue : x));
     }
 
-    const handleSubmit = () => {
-        setResult(values.join(""));
-        if (validateOTP(result))
-            alert("correct");
+    const initialize = useCallback(() => {
         setValues(["", "", "", "", "", ""]);
         const first = ref1.current;
         if (first) {
             first.disabled = false;
             first.focus();
         }
-    }
+    }, []);
+
+    const resolve = useCallback(() => {
+        alert(`correct ${values.join("")}`)
+    }, [values]);
+
+    useEffect(() => {
+        if (values[5] !== "") {
+            if (validateOTP(values.join(""))) {
+                resolve();
+            }
+            initialize();
+        }
+    }, [initialize, resolve, values]);
 
     return (
         <main>
@@ -135,9 +129,9 @@ export default function LoginPage() {
                         <DigitBlock prev={ref2} curr={ref3} next={ref4} value={values[2]} setValue={setValuesAt(2)} />
                         <DigitBlock prev={ref3} curr={ref4} next={ref5} value={values[3]} setValue={setValuesAt(3)} />
                         <DigitBlock prev={ref4} curr={ref5} next={ref6} value={values[4]} setValue={setValuesAt(4)} />
-                        <DigitBlock prev={ref5} curr={ref6} next={null} value={values[5]} setValue={setValuesAt(5)} onSubmit={handleSubmit} />
+                        <DigitBlock prev={ref5} curr={ref6} next={null} value={values[5]} setValue={setValuesAt(5)} />
                     </div>
-                    <p>{result}</p>
+                    <p>{`result: ${values.join("")}`}</p>
                 </div>
             </div>
         </main>
