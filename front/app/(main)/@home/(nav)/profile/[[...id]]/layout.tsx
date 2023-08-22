b5eed1169014efdd9dad8c310dea56c8611fd623
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-08-22 22:08:56 +0900

    media quary

--- front/app/(main)/@home/(nav)/profile/[[...id]]/layout.tsx
+++ front/app/(main)/@home/(nav)/profile/[[...id]]/layout.tsx
@@ -1,7 +1,7 @@
 import { ProfileSection } from "@/components/Profile/ProfileSection";
 export default function ProfileLayout({ children }: React.PropsWithChildren) {
     return (
-        <div className="flex h-full w-full flex-col items-start bg-black/30 backdrop-blur-[50px] 2xl:flex-row">
+        <div className="flex h-full w-full flex-col items-start bg-windowGlass/30 backdrop-blur-[50px] xl:flex-row">
             <ProfileSection />
             {children}
         </div>
--- front/app/(main)/@home/(nav)/profile/[[...id]]/page.tsx
+++ front/app/(main)/@home/(nav)/profile/[[...id]]/page.tsx
@@ -5,7 +5,7 @@ import { Panel } from "@/components/Profile/Panel";
 
 export default function ProfilePage() {
     return (
-        <div className="grid h-full w-full gap-8 overflow-auto p-8 md:grid-cols-2 2xl:grid-cols-3">
+        <div className="grid h-full w-full gap-8 overflow-auto p-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
             <AchievementPenal />
             <Panel className=""></Panel>
             <Panel className=""></Panel>
