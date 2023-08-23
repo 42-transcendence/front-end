998cd2eae8a25aa1d3495e3d3f07a9bd15771a6b
jkong <jkong@student.42seoul.kr>
2023-08-23 20:12:26 +0900

    OTP 입력과 Welcome 페이지를 패러렐 라우팅으로 지정함.
    총 6가지 상태가 필요함.
    1. 로그인 안된 상태
    2. OTP 입력이 필요한 상태
    3. 서비스 정지 당한 상태
    4. 탈퇴 유예 기간인 상태
    5. 최초 가입자 (nickName === null)
    6. 정상 로그인

--- front/app/(main)/layout.tsx
+++ front/app/(main)/layout.tsx
@@ -2,40 +2,15 @@
 
 import { useState, useEffect } from "react";
 
-function setCookie(
-    name: string,
-    value: string,
-    options: Record<string, any> & { expires?: Date | string | undefined } = {},
-) {
-    options = {
-        path: "/",
-        // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
-        ...options,
-    };
-
-    if (options.expires instanceof Date) {
-        options.expires = options.expires.toUTCString();
-    }
-
-    let updatedCookie =
-        encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-    for (const optionKey in options) {
-        updatedCookie += "; " + optionKey;
-        const optionValue = options[optionKey];
-        if (optionValue !== true) {
-            updatedCookie += "=" + optionValue;
-        }
-    }
-
-    document.cookie = updatedCookie;
-}
-
 export default function MainLayout({
     login,
+    otp,
+    welcome,
     home,
 }: {
     login: React.ReactNode;
+    otp: React.ReactNode;
+    welcome: React.ReactNode;
     home: React.ReactNode;
 }) {
     const [loggedin, setLoggedin] = useState<boolean>(false);
@@ -45,9 +20,6 @@ export default function MainLayout({
             //TODO: 기존에 이미 인증된 상태(스토리지에 토큰이 있는 상태)라면 백으로 한번 보내서 검증시켜봐야 함.
             const accessToken = window.localStorage.getItem("access_token");
             setLoggedin(accessToken !== null);
-            if (accessToken !== null) {
-                setCookie("at", accessToken, { secure: true, "max-age": 3600 });
-            }
         }
 
         checkAccessToken();
@@ -64,8 +36,6 @@ export default function MainLayout({
             window.removeEventListener("storage", receiveMessage);
         };
     }, [loggedin]);
-
-    console.log(Object.keys(home ?? {}), Object.keys(login?? {}))
 
     return (
         <div className="flex h-[100dvh] flex-shrink-0 flex-col">
