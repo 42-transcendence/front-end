cf0e9356581cb81f27ae108a0d3eb8cc7599e73e
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-08-27 20:50:27 +0900

    Add z-index to input boxes

--- front/app/(main)/@otp/page.tsx
+++ front/app/(main)/@otp/page.tsx
@@ -3,18 +3,18 @@ import { OTPInputBlocks } from "./OTPInputBlocks";
 export default function LoginPage() {
     return (
         <main>
-            <div className="flex justify-center w-full h-[100dvh]">
-                <div className="flex relative flex-col justify-center h-full">
-                    <div className="flex flex-col gap-8 justify-center items-center p-16 py-32 backblur gradient-border h-fit w-fit rounded-[28px] bg-windowGlass/30 backdrop-blur-[50px] before:rounded-[28px] before:p-px">
+            <div className="flex h-[100dvh] w-full justify-center">
+                <div className="relative flex h-full flex-col justify-center">
+                    <div className="backblur gradient-border flex h-fit w-fit flex-col items-center justify-center gap-8 rounded-[28px] bg-windowGlass/30 p-16 py-32 backdrop-blur-[50px] before:rounded-[28px] before:p-px">
                         <div>
-                            <span className="flex flex-col justify-center w-96 h-10 text-3xl font-bold text-center text-white">
+                            <span className="flex h-10 w-96 flex-col justify-center text-center text-3xl font-bold text-white">
                                 Verify your Number
                             </span>
-                            <span className="flex flex-col justify-center self-stretch text-base not-italic font-bold text-center shrink-0 text-gray-50/70">
+                            <span className="flex shrink-0 flex-col justify-center self-stretch text-center text-base font-bold not-italic text-gray-50/70">
                                 Open the authenticator and enter the number
                             </span>
                         </div>
-                        <div className="flex gap-2 items-start">
+                        <div className="z-10 flex items-start gap-2">
                             <OTPInputBlocks length={6} />
                         </div>
                     </div>
