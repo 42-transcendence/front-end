1c48cb4f3b0aaf09051ee0fcd874fded4a2eb17a
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-08-27 18:13:56 +0900

    Add TODO to layout

--- front/app/profile/[[...id]]/page.tsx
+++ front/app/profile/[[...id]]/page.tsx
@@ -8,6 +8,7 @@ export default function ProfilePage() {
     return (
         <div className="grid overflow-auto grid-cols-1 gap-8 p-4 w-full h-full md:grid-cols-2 lg:grid-cols-4 lg:p-8">
             <Panel className="md:col-span-2 lg:col-span-4 lg:row-span-2"></Panel>
+            {/*TODO: put accountUUID of selected user */}
             <AchievementPenal accountUUID={""} />
             <GameHistoryPenal accountUUID={""} />
         </div>
