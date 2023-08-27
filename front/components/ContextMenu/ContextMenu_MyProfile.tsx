e4bfb89b4220904229a23a56dc7b89101753f2ef
El <72256725+ElApostles@users.noreply.github.com>
2023-08-27 22:43:10 +0900

    Update ContextMenu_MyProfile.tsx

--- front/components/ContextMenu/ContextMenu_MyProfile.tsx
+++ front/components/ContextMenu/ContextMenu_MyProfile.tsx
@@ -3,12 +3,12 @@ import type { ProfileItemConfig } from "./ContextMenuBase_Profile";
 import { ContextMenuItem } from "./ContextMenuItem";
 
 export function ContextMenu_MyProfile({
-    info: profile,
+    info,
 }: {
     info: ProfileItemConfig;
 }) {
     return (
-        <ContextMenuBase_Profile profile={profile}>
+        <ContextMenuBase_Profile profile={info}>
             <ContextMenuItem name="내 상태 변경하기" className="basic" />
             <ContextMenuItem name="내 정보 수정하기" className="basic" />
             <ContextMenuItem name="로그아웃 하기" className="basic" />
