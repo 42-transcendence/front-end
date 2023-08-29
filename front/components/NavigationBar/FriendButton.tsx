16e5af1b2bb6c00c111a79e3edc94879eba80de8
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-08-29 20:44:31 +0900

    Add simple badge

--- front/components/NavigationBar/FriendButton.tsx
+++ front/components/NavigationBar/FriendButton.tsx
@@ -3,6 +3,8 @@
 import { forwardRef, useRef } from "react";
 import { Icon } from "../ImageLibrary";
 import { FriendModal } from "../FriendModal/FriendModal";
+import { useAtomValue } from "jotai";
+import { FriendRequestEntryAtom } from "@/atom/FriendAtom";
 
 const ModalComponent = forwardRef(function ModalComponent(
     { children, className }: { children: React.ReactNode; className: string },
@@ -24,16 +26,25 @@ export function FriendButton() {
         }
         dialogRef.current.showModal();
     };
+    // useOutsideClick(dialogRef, () => dialogRef.current?.close(), true);
+    const accountUUIDs = useAtomValue(FriendRequestEntryAtom);
 
     return (
         <>
             <ModalComponent className="bg-transparent" ref={dialogRef}>
                 <FriendModal />
             </ModalComponent>
-            <Icon.Members
-                onClick={showModal}
-                className="h-12 w-12 rounded-lg p-3 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14"
-            />
+            <div className="relative flex h-fit w-fit">
+                <Icon.Members
+                    onClick={showModal}
+                    className="h-12 w-12 rounded-lg p-3 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14"
+                />
+                {accountUUIDs.length !== 0 && (
+                    <div className="absolute right-2 top-2 flex h-fit w-fit rounded-lg bg-red-500/90 p-1">
+                        <div className="h-1 w-1 rounded-full bg-white"></div>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
--- front/components/NavigationBar/SocialButton.tsx
+++ front/components/NavigationBar/SocialButton.tsx
@@ -18,12 +18,23 @@ export function SocialButton() {
 
     // TODO: href 이동으로 인한 pathname 업데이트와 handleClick 내부
     // setPrevPathname 사이의 실행순서가 보장되는가?
+
+    // TODO: get from getlastreadmessage that unreaded messages
+    const unreadedMessages = 123;
+
     return (
         <Link
             href={isOnChatPage ? prevPathname : "/chat"}
             onClick={handleClick}
         >
-            <ChatIcon className="h-12 w-12 rounded-lg p-2 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14" />
+            <div className="relative flex rounded-lg hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary">
+                <ChatIcon className="h-12 w-12  p-2 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30]  2xl:h-14 2xl:w-14" />
+                <div className="absolute right-0 top-0 flex h-fit w-fit min-w-[1.25rem] justify-center rounded-lg bg-red-500/90 p-1">
+                    <span className="rounded-full text-xs leading-3">
+                        {unreadedMessages}
+                    </span>
+                </div>
+            </div>
         </Link>
     );
 }
