4124a97ff1b3efc634e3b039f23fa21215a99f21
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-08-30 22:29:10 +0900

    Remove extra page

--- front/app/(main)/@home/(nav)/dropdown/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FriendModal } from "@/components/FriendModal";
-
-export default function Home() {
-    return (
-        <main className="min flex h-full flex-col items-center justify-between p-24">
-            <FriendModal />
-        </main>
-    );
-}
