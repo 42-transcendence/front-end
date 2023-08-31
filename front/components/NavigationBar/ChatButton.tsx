d68ed031d231a7cafb8e11fd9c887df4b352a922
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-08-31 20:53:38 +0900

    Change Chat icon

--- front/components/NavigationBar/ChatButton.tsx
+++ front/components/NavigationBar/ChatButton.tsx
@@ -6,11 +6,14 @@ import { useAtomValue } from "jotai";
 import { FriendRequestEntryAtom } from "@/atom/FriendAtom";
 import { Dialog } from "@headlessui/react";
 import ChatLayout from "../Chat/ChatLayout";
+import { usePathname } from "next/navigation";
 
 export function ChatButton() {
     const accountUUIDs = useAtomValue(FriendRequestEntryAtom);
     const [isOpen, setIsOpen] = useState(false);
     const ref = useRef<HTMLDivElement>(null);
+
+    const ChatIcon = isOpen ? Icon.ChatFilled : Icon.ChatOutlined;
 
     return (
         <>
@@ -27,9 +30,9 @@ export function ChatButton() {
                 </Dialog.Panel>
             </Dialog>
             <div ref={ref} className="relative flex h-fit w-fit">
-                <Icon.Members
+                <ChatIcon
                     onClick={() => setIsOpen(true)}
-                    className="h-12 w-12 rounded-lg p-3 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14"
+                    className="h-12 w-12 rounded-lg p-2 shadow-white drop-shadow-[0_0_0.1rem_#ffffff30] hover:bg-primary/30 hover:text-white/80 focus:bg-controlsSelected active:bg-secondary 2xl:h-14 2xl:w-14"
                 />
                 {accountUUIDs.length !== 0 && (
                     <div className="absolute right-2 top-2 flex h-fit w-fit rounded-lg bg-red-500/90 p-1">
