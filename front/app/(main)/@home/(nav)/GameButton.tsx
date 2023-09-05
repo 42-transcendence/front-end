e0ccfb1ca2ea61562bb97502547609c2fd7a14f4
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-05 23:51:04 +0900

    extract to roundbutton from gameButton

--- front/app/(main)/@home/(nav)/GameButton.tsx
+++ front/app/(main)/@home/(nav)/GameButton.tsx
@@ -1,27 +1,10 @@
 "use client";
 
+import { RoundButtonBase } from "@/components/Button/RoundButton";
 import { useRouter } from "next/navigation";
 
-// TODO: 나중에 잘 정리해서 컴포넌트로 만들기 가능할듯?
-function GameButtonBase({
-    children,
-    ...props
-}: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
-    return (
-        <button
-            type="button"
-            className="relative flex place-items-center"
-            {...props}
-        >
-            <div className="gradient-border group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[50px] transition-colors before:pointer-events-none before:absolute before:inset-0 before:rounded-[28px] before:p-px before:backdrop-brightness-100 before:content-[''] hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
-                <h2 className="text-sans text-2xl font-medium">{children}</h2>
-            </div>
-        </button>
-    );
-}
-
 export function CreateGameButton() {
-    return <GameButtonBase>Create Game</GameButtonBase>;
+    return <RoundButtonBase>Create Game</RoundButtonBase>;
 }
 
 export function QuickMatchButton() {
@@ -48,5 +31,5 @@ export function QuickMatchButton() {
     //     router.push(`/game/${newGameId}`);
     // }
 
-    return <GameButtonBase onClick={asyncRoute}>Quick Match</GameButtonBase>;
+    return <RoundButtonBase onClick={asyncRoute}>Quick Match</RoundButtonBase>;
 }
---
+++ front/components/Button/RoundButton.tsx
@@ -0,0 +1,18 @@
+export function RoundButtonBase({
+    children,
+    ...props
+}: React.PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
+    return (
+        <button
+            type="button"
+            className="relative flex place-items-center"
+            {...props}
+        >
+            <div className="gradient-border back-full group relative flex w-[262px] flex-col items-start gap-2 rounded-[28px] border-transparent bg-windowGlass/30 p-px px-5 py-4 backdrop-blur-[50px] transition-colors before:pointer-events-none before:rounded-[28px] before:p-px before:backdrop-brightness-100 hover:bg-primary focus:outline-2 focus:outline-gray-100 focus:ring focus:ring-violet-300 active:bg-violet-700 hover:dark:border-purple-700 hover:dark:bg-neutral-800/30">
+                <span className="text-sans text-2xl font-medium">
+                    {children}
+                </span>
+            </div>
+        </button>
+    );
+}
