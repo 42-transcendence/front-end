50cd1befeec8a66fa4dfe630a19b18a27c1f4a55
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-05 23:52:28 +0900

    Allow profile pages to be routed through nicknames and tags.

--- front/app/profile/[[...id]]/page.tsx
+++ front/app/profile/[[...id]]/page.tsx
@@ -1,44 +1,82 @@
 "use client";
 
 import { editPanelVisibilityAtom } from "@/atom/ProfileAtom";
+import { RoundButtonBase } from "@/components/Button/RoundButton";
 import { AchievementPanel } from "@/components/Profile/AchievementPanel";
 import { EditPanel } from "@/components/Profile/EditPanel";
 import { GameHistoryPanel } from "@/components/Profile/GameHistoryPanel";
 import { ProfileSection } from "@/components/Profile/ProfileSection";
+import { useNickLookup, usePrivateProfile } from "@/hooks/useProfile";
+import { NICK_NAME_REGEX } from "@/library/payload/profile-constants";
 import { useAtomValue } from "jotai";
-import { useParams } from "next/navigation";
+import Link from "next/link";
 
 function ErrorPage() {
-    return <div>invalid profile id</div>;
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center gap-16">
+            <span>Page not found</span>
+            <Link className="relative" href="/">
+                <RoundButtonBase>Back To Home</RoundButtonBase>
+            </Link>
+        </div>
+    );
 }
 
-function isValidProfileId(id: string | string[]) {
-    if (!Array.isArray(id)) return false;
-    return id.length === 1 && Number.isInteger(Number(id));
+function isValidProfileId(params: string[] | undefined) {
+    if (!Array.isArray(params)) return false;
+    const regexp = NICK_NAME_REGEX;
+    return (
+        params.length === 2 &&
+        regexp.test(params[0]) &&
+        Number.isInteger(Number(params[1]))
+    );
 }
 
-export default function ProfilePage() {
-    const params = useParams();
+export default function ProfilePage({
+    params,
+}: {
+    params: { id: string[] | undefined };
+}) {
+    const profile = usePrivateProfile();
+    const nick =
+        params.id !== undefined ? params.id[0] : profile?.nickName ?? "";
+    const tag =
+        params.id !== undefined ? Number(params.id[1]) : profile?.nickTag ?? 0;
+    const result = useNickLookup(nick, tag);
     const editPanelVisibility = useAtomValue(editPanelVisibilityAtom);
+    const accountUUID = result.accountUUID;
 
-    // TODO: 실제 로직으로 변경. game id 쪽 일단 복붙해옴. 같이 수정 요망
+    if (profile === undefined) {
+        return <ErrorPage />;
+    }
+
+    if (result.notFound) {
+        return <ErrorPage />;
+    }
+    if (result.isLoading) {
+        return (
+            <>
+                <ProfileSection accountUUID={accountUUID} />
+                <div className="grid h-full w-full grid-cols-1 gap-8 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-4 lg:p-8">
+                    {editPanelVisibility && <EditPanel />}
+                    <AchievementPanel accountUUID={accountUUID} />
+                </div>
+            </>
+        );
+    }
     const paramKeys = Object.keys(params);
     if (paramKeys.length !== 1 || paramKeys[0] !== "id") {
         return <ErrorPage />;
     }
-
     if (!isValidProfileId(params.id)) {
         return <ErrorPage />;
     }
 
-    // TODO: change to real uuid
-    const accountUUID = params.id as string;
     return (
         <>
             <ProfileSection accountUUID={accountUUID} />
             <div className="grid h-full w-full grid-cols-1 gap-8 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-4 lg:p-8">
                 {editPanelVisibility && <EditPanel />}
-                {/*TODO: put accountUUID of selected user */}
                 <AchievementPanel accountUUID={accountUUID} />
                 <GameHistoryPanel accountUUID={accountUUID} />
             </div>
