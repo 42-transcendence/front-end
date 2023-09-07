657b431a56a8911d507753f65619044f5c425fbc
Hyeokjin Doo <hdoo@c1r1s5.42seoul.kr>
2023-09-07 17:22:44 +0900

    Fix login

--- front/app/(main)/layout.tsx
+++ front/app/(main)/layout.tsx
@@ -45,12 +45,6 @@ export default function MainLayout({
     welcome: React.ReactNode;
     home: React.ReactNode;
 }) {
-    return (
-        <DefaultLayout>
-            <ChatSocketProcessor />
-            {home}
-        </DefaultLayout>
-    );
     const [hydrated, setHydrated] = useState(false);
     useEffect(() => {
         setHydrated(true);
@@ -143,4 +137,10 @@ export default function MainLayout({
     }
 
     // 넌 최고야!
+    return (
+        <DefaultLayout>
+            <ChatSocketProcessor />
+            {home}
+        </DefaultLayout>
+    );
 }
