50878387b1c7426056557cdeae2c794d88af77a3
chanhpar <chanhpar@student.42seoul.kr>
2023-09-07 20:00:56 +0900

    profile page refactoring

--- front/app/profile/[[...id]]/page.tsx
+++ front/app/profile/[[...id]]/page.tsx
@@ -8,6 +8,7 @@ import { GameHistoryPanel } from "@/components/Profile/GameHistoryPanel";
 import { ProfileSection } from "@/components/Profile/ProfileSection";
 import { useNickLookup, usePrivateProfile } from "@/hooks/useProfile";
 import { NICK_NAME_REGEX } from "@/library/payload/profile-constants";
+import type { AccountProfileProtectedPayload } from "@/library/payload/profile-payloads";
 import { useAtomValue } from "jotai";
 import Link from "next/link";
 
@@ -32,43 +33,42 @@ function isValidProfileId(params: string[] | undefined) {
     );
 }
 
+function parseNickAndTag(
+    params: { id?: string[] | undefined },
+    profile: AccountProfileProtectedPayload | undefined,
+): [nick: string, tag: number, isValid: boolean] {
+    const paramKeys = Object.keys(params);
+    const isValid =
+        profile !== undefined &&
+        paramKeys.length === 1 &&
+        paramKeys[0] === "id" &&
+        isValidProfileId(params.id);
+
+    const [nick, tag] =
+        params.id !== undefined
+            ? [params.id[0], Number(params.id[1])]
+            : profile !== undefined
+            ? [profile.nickName ?? "", profile.nickTag]
+            : ["", 0];
+
+    return [nick, tag, isValid];
+}
+
 export default function ProfilePage({
     params,
 }: {
     params: { id?: string[] | undefined };
 }) {
     const profile = usePrivateProfile();
-    const nick =
-        params.id !== undefined ? params.id[0] : profile?.nickName ?? "";
-    const tag =
-        params.id !== undefined ? Number(params.id[1]) : profile?.nickTag ?? 0;
-    const result = useNickLookup(nick, tag);
+    const [nick, tag, isValid] = parseNickAndTag(params, profile);
+    const { accountUUID, isLoading, notFound } = useNickLookup(nick, tag);
     const editPanelVisibility = useAtomValue(EditPanelVisibilityAtom);
-    const accountUUID = result.accountUUID;
 
-    if (profile === undefined) {
+    if (!isValid) {
         return <ErrorPage />;
     }
 
-    if (result.notFound) {
-        return <ErrorPage />;
-    }
-    if (result.isLoading) {
-        return (
-            <>
-                <ProfileSection accountUUID={accountUUID} />
-                <div className="grid h-full w-full grid-cols-1 gap-8 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-4 lg:p-8">
-                    {editPanelVisibility && <EditPanel />}
-                    <AchievementPanel accountUUID={accountUUID} />
-                </div>
-            </>
-        );
-    }
-    const paramKeys = Object.keys(params);
-    if (paramKeys.length !== 1 || paramKeys[0] !== "id") {
-        return <ErrorPage />;
-    }
-    if (!isValidProfileId(params.id)) {
+    if (notFound) {
         return <ErrorPage />;
     }
 
@@ -78,7 +78,7 @@ export default function ProfilePage({
             <div className="grid h-full w-full grid-cols-1 gap-8 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-4 lg:p-8">
                 {editPanelVisibility && <EditPanel />}
                 <AchievementPanel accountUUID={accountUUID} />
-                <GameHistoryPanel accountUUID={accountUUID} />
+                {!isLoading && <GameHistoryPanel accountUUID={accountUUID} />}
             </div>
         </>
     );
