4056c0d0926cfe61bfc0339cc72e3bcff79d7502
Insung Yun <iyun@c1r1s7.42seoul.kr>
2023-09-08 17:52:58 +0900

    game

--- front/app/(main)/@home/(nav)/GameButton.tsx
+++ front/app/(main)/@home/(nav)/GameButton.tsx
@@ -1,10 +1,39 @@
 "use client";
 
-import { RoundButtonBase } from "@components/Button/RoundButton";
+import { useCurrentAccountUUID } from "@/hooks/useCurrent";
+import { ByteBuffer } from "@/library/akasha-lib";
+import { ChatStore } from "@/library/idb/chat-store";
+import { ChatServerOpcode } from "@/library/payload/chat-opcodes";
+import { ChatRoomChatMessagePairEntry, writeChatRoomChatMessagePair } from "@/library/payload/chat-payloads";
+import { GameClientOpcode, GameServerOpcode } from "@/library/payload/game-opcodes";
+import { useWebSocket, useWebSocketConnector } from "@/library/react/websocket-hook";
 import { useRouter } from "next/navigation";
+import { useMemo } from "react";
 
 export function CreateGameButton() {
-    return <RoundButtonBase>Create Game</RoundButtonBase>;
+    const router = useRouter();
+    //TODO: make form for new game room
+    const currentAccountUUID = useCurrentAccountUUID();
+    const props = useMemo(
+        () => ({
+            handshake: async () => {
+                const buf = ByteBuffer.createWithOpcode(GameServerOpcode.CREATE);
+                return buf.toArray();
+            },
+        }),
+        [currentAccountUUID],
+    );
+
+    useWebSocketConnector("game", "ws://localhost:3001/game", props);
+    const { sendPayload } = useWebSocket("game", [GameClientOpcode.ACCEPT], async (opcode, buf) => {
+        const uuid = buf.readUUID();
+        //TODO: UUID to hash value that indicate game room.
+        router.push(`/game/${uuid}`);
+    });
+
+    void sendPayload;
+
+    return <GameButtonBase >Create Game</GameButtonBase>;
 }
 
 export function QuickMatchButton() {
@@ -14,6 +43,7 @@ export function QuickMatchButton() {
     const asyncRoute = async () => {
         await new Promise((res) => setTimeout(res, 0));
         router.push("/game/4242");
+
     };
 
     // TODO: 실제로는 백에서 채팅방 받아와야. fetch할때 방 정보, 모드 등 parameter로 붙여서 요청
