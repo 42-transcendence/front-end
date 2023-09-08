24717f977d6a9442919e23fb5a528f768cada988
jkong <jkong@student.42seoul.kr>
2023-09-08 14:43:58 +0900

    슈퍼 포스 메인 파워 푸시!! 얍!! common payload 업데이트

--- front/library/payload/profile-constants.ts
+++ front/library/payload/profile-constants.ts
@@ -1,4 +1,6 @@
 export const NICK_NAME_PATTERN = "[a-zA-Z0-9가-힣]{2,8}";
+export const MIN_NICK_TAG_NUMBER = 1000;
+export const MAX_NICK_TAG_NUMBER = 9999;
 export const AVATAR_FORM_DATA_KEY = "avatar";
 export const AVATAR_MIME_TYPE = "image/webp";
 export const AVATAR_LIMIT = 1 * 1024 * 1024; //NOTE: 1MB
