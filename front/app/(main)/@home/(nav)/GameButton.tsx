6c34b66edd669253430ee3eb55d8b07a9bf9353f
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-09 16:28:28 +0900

    Create new Game Room UI

--- front/app/(main)/@home/(nav)/GameButton.tsx
+++ front/app/(main)/@home/(nav)/GameButton.tsx
@@ -1,39 +1,32 @@
 "use client";
 
-import { useCurrentAccountUUID } from "@/hooks/useCurrent";
-import { ByteBuffer } from "@/library/akasha-lib";
-import { ChatStore } from "@/library/idb/chat-store";
-import { ChatServerOpcode } from "@/library/payload/chat-opcodes";
-import { ChatRoomChatMessagePairEntry, writeChatRoomChatMessagePair } from "@/library/payload/chat-payloads";
-import { GameClientOpcode, GameServerOpcode } from "@/library/payload/game-opcodes";
-import { useWebSocket, useWebSocketConnector } from "@/library/react/websocket-hook";
 import { useRouter } from "next/navigation";
-import { useMemo } from "react";
+import { useState } from "react";
+import { RoundButtonBase } from "@components/Button/RoundButton";
+import { GlassWindow } from "@components/Frame/GlassWindow";
+import { TextField } from "@components/TextField";
 
 export function CreateGameButton() {
-    const router = useRouter();
-    //TODO: make form for new game room
-    const currentAccountUUID = useCurrentAccountUUID();
-    const props = useMemo(
-        () => ({
-            handshake: async () => {
-                const buf = ByteBuffer.createWithOpcode(GameServerOpcode.CREATE);
-                return buf.toArray();
-            },
-        }),
-        [currentAccountUUID],
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="flex h-fit w-full flex-col gap-4 lg:flex-row">
+            <RoundButtonBase onClick={() => setOpen(!open)}>
+                Create Game
+            </RoundButtonBase>
+            {open && <CreateNewGameRoom />}
+        </div>
     );
+}
 
-    useWebSocketConnector("game", "ws://localhost:3001/game", props);
-    const { sendPayload } = useWebSocket("game", [GameClientOpcode.ACCEPT], async (opcode, buf) => {
-        const uuid = buf.readUUID();
-        //TODO: UUID to hash value that indicate game room.
-        router.push(`/game/${uuid}`);
-    });
-
-    void sendPayload;
-
-    return <GameButtonBase >Create Game</GameButtonBase>;
+function CreateNewGameRoom() {
+    return (
+        <GlassWindow>
+            <div className="flex h-full w-full flex-row items-center justify-start gap-4 p-4">
+                <span>Title</span>
+                <TextField className="w-full" />
+            </div>
+        </GlassWindow>
+    );
 }
 
 export function QuickMatchButton() {
@@ -43,7 +36,6 @@ export function QuickMatchButton() {
     const asyncRoute = async () => {
         await new Promise((res) => setTimeout(res, 0));
         router.push("/game/4242");
-
     };
 
     // TODO: 실제로는 백에서 채팅방 받아와야. fetch할때 방 정보, 모드 등 parameter로 붙여서 요청
