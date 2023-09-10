ab2ba6323b14ae4567114a8908060253d1437dec
chanhpar <chanhpar@student.42seoul.kr>
2023-09-10 18:42:21 +0900

    component 옛날파일 삭제. 새 파일에 변경사항 반영

--- front/components/Chat/ChatMainPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChatMainPage({ children }: React.PropsWithChildren) {
-    return (
-        <div className="flex-grow-1 relative flex h-full w-full flex-col items-start bg-transparent transition-all 2xl:gap-4 2xl:p-4">
-            {children}
-        </div>
-    );
-}
