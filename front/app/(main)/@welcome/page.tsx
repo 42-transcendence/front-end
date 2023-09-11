6c8f92f6a1106323f0ac6eb21f5b772856d6b3d5
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-11 09:22:40 +0900

    improve welcome page

--- front/app/(main)/@welcome/page.tsx
+++ front/app/(main)/@welcome/page.tsx
@@ -21,22 +21,41 @@ export default function Welcome() {
                     <DoubleSharp width="24" height="24" />
                     <p>사용할 닉네임을 입력해주세요.</p>
                 </div>
-                <div className="flex flex-row gap-2">
-                    <TextField
-                        className="peer p-3"
-                        value={value}
-                        pattern="[a-zA-Z0-9가-힣]{2,8}"
-                        placeholder="닉네임을 입력해주세요!"
-                        required
-                        onChange={(event) => setValue(event.target.value)}
-                    />
+                <form
+                    onClick={(e) => e.preventDefault()}
+                    className="group relative flex flex-row gap-2"
+                >
+                    <div className="flex min-w-[12rem] max-w-xs flex-col">
+                        <TextField
+                            className="peer p-3"
+                            value={value}
+                            onKeyDown={(e) =>
+                                e.key === "Enter" && void register(value)
+                            }
+                            pattern="[a-zA-Z0-9가-힣]{2,8}"
+                            placeholder="닉네임을 입력해주세요!"
+                            required
+                            onChange={(event) => setValue(event.target.value)}
+                        />
+                        {error && (
+                            <span className="p-2 text-sm text-red-500/90">
+                                오류가 발생했습니다. 다시 시도해주세요.
+                            </span>
+                        )}
+                        {conflict && (
+                            <span className="p-2 text-sm text-red-500/90">
+                                이미 9000명이 쓰는 흔해빠진 닉네임인데, 다른걸로
+                                하시는게 어떤지..? 감히? 고견? 드립? 니다?
+                            </span>
+                        )}
+                    </div>
                     <button
-                        className="z-10 h-8 w-8 shrink-0 items-center justify-center rounded bg-gray-500/80 p-2 text-gray-200/50 transition-colors duration-300 hover:bg-primary hover:text-white"
+                        className="z-10 h-8 w-8 shrink-0 items-center justify-center rounded bg-gray-500/80 p-2 text-gray-200/50 transition-colors duration-300 hover:bg-primary hover:text-white group-valid:bg-green-500/50 group-valid:text-white"
                         onClick={() => void register(value)}
                     >
                         <Icon.Arrow3 className="" />
                     </button>
-                </div>
+                </form>
                 {/* TODO : 서버에서 닉네임이 중복되었는지, 가능한 닉네임인지 확인 */}
             </Card>
         </main>
--- front/app/__libs__/components/Card/Card.tsx
+++ front/app/__libs__/components/Card/Card.tsx
@@ -7,7 +7,7 @@ export function Card({
 }) {
     return (
         <div
-            className={`${className} gradient-border before:contents-[''] flex h-fit w-fit flex-col items-center justify-between gap-8 rounded-[28px] bg-windowGlass/30 p-32 backdrop-blur-[50px] before:absolute before:inset-0 before:rounded-[28px] before:p-px`}
+            className={`${className} gradient-border before:contents-[''] flex h-fit w-fit flex-col items-center justify-between gap-8 rounded-[28px] bg-windowGlass/30 p-24 backdrop-blur-[50px] before:absolute before:inset-0 before:rounded-[28px] before:p-px`}
         >
             {children}
         </div>
