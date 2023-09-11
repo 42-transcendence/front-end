b8e5c0f362674df172a81458f90eb4e500388314
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-11 10:36:20 +0900

    Chat Ban list

--- front/app/__libs__/components/Chat/ChatBanList.tsx
+++ front/app/__libs__/components/Chat/ChatBanList.tsx
@@ -1,56 +1,14 @@
 import { useEffect, useState } from "react";
 import { ProfileItemBlocked } from "@components/ProfileItem/ProfileItemBlocked";
-import { makeBanListRequest } from "@akasha-utils/chat-payload-builder-client";
+import { BanCategoryNumber } from "@common/generated/types";
+import type { ChatBanDetailEntry } from "@common/chat-payloads";
 import { useWebSocket } from "@akasha-utils/react/websocket-hook";
 import { ChatClientOpcode } from "@common/chat-opcodes";
+import { makeBanListRequest } from "@akasha-utils/chat-payload-builder-client";
 import { useCurrentChatRoomUUID } from "@hooks/useCurrent";
 import { handleBanList } from "@akasha-utils/chat-gateway-client";
-import { BanCategory, BanCategoryNumber } from "@common/generated/types";
-import { ChatBanDetailEntry } from "@common/chat-payloads";
-
-type BlockedUser = {
-    accountUUID: string;
-    memo: string;
-    expireTimeStamp: Date;
-};
-const CommitBanList: BlockedUser[] = [
-    {
-        accountUUID: "123456",
-        memo: "그냥",
-        expireTimeStamp: new Date(),
-    },
-    {
-        accountUUID: "47789",
-        memo: "그냥2",
-        expireTimeStamp: new Date(),
-    },
-    {
-        accountUUID: "5123",
-        memo: "그냥3",
-        expireTimeStamp: new Date(),
-    },
-];
-
-const AccessBanList: BlockedUser[] = [
-    {
-        accountUUID: "123",
-        memo: "그냥",
-        expireTimeStamp: new Date(),
-    },
-    {
-        accountUUID: "4",
-        memo: "그냥2",
-        expireTimeStamp: new Date(),
-    },
-    {
-        accountUUID: "5",
-        memo: "그냥3",
-        expireTimeStamp: new Date(),
-    },
-];
 
 export function ChatAccessBanList() {
-    const chatId = useCurrentChatRoomUUID();
     const [accessBanlist, setAccessBanList] = useState<ChatBanDetailEntry[]>();
     const { sendPayload } = useWebSocket(
         "chat",
@@ -64,19 +22,19 @@ export function ChatAccessBanList() {
         },
     );
 
-    useEffect(() => {
-        sendPayload(makeBanListRequest(chatId));
-    }, [chatId, sendPayload]);
-
     const [selected, setSelected] = useState<number>();
+    const currentChatRoomId = useCurrentChatRoomUUID();
+    useEffect(() => {
+        sendPayload(makeBanListRequest(currentChatRoomId));
+    }, [currentChatRoomId, sendPayload]);
 
     return (
         <div className="flex h-fit w-full flex-col gap-2">
-            {accessBanlist?.map((detail, index) => {
+            {accessBanlist?.map((entry, index) => {
                 return (
                     <ProfileItemBlocked
                         key={index}
-                        detail={detail}
+                        entry={entry}
                         selected={selected === index}
                         onClick={() =>
                             setSelected(selected !== index ? index : undefined)
@@ -89,13 +47,12 @@ export function ChatAccessBanList() {
 }
 
 export function ChatCommitBanList() {
-    const chatId = useCurrentChatRoomUUID();
-    const [CommitBanList, setCommitBanList] = useState<ChatBanDetailEntry[]>();
+    const [sendBanList, setSendBanList] = useState<ChatBanDetailEntry[]>();
     const { sendPayload } = useWebSocket(
         "chat",
         ChatClientOpcode.BAN_LIST,
         (_, buffer) => {
-            setCommitBanList(
+            setSendBanList(
                 handleBanList(buffer).filter(
                     (e) => e.category === BanCategoryNumber.COMMIT,
                 ),
@@ -103,19 +60,19 @@ export function ChatCommitBanList() {
         },
     );
 
-    useEffect(() => {
-        sendPayload(makeBanListRequest(chatId));
-    }, [chatId, sendPayload]);
-
     const [selected, setSelected] = useState<number>();
+    const currentChatRoomId = useCurrentChatRoomUUID();
+    useEffect(() => {
+        sendPayload(makeBanListRequest(currentChatRoomId));
+    }, [currentChatRoomId, sendPayload]);
 
     return (
         <div className="flex h-fit w-full flex-col gap-2">
-            {CommitBanList?.map((detail, index) => {
+            {sendBanList?.map((entry, index) => {
                 return (
                     <ProfileItemBlocked
                         key={index}
-                        detail={detail}
+                        entry={entry}
                         selected={selected === index}
                         onClick={() =>
                             setSelected(selected !== index ? index : undefined)
--- front/app/__libs__/components/ProfileItem/ProfileItemBlocked.tsx
+++ front/app/__libs__/components/ProfileItem/ProfileItemBlocked.tsx
@@ -1,32 +1,33 @@
 import { handleUnbanMemberResult } from "@akasha-utils/chat-gateway-client";
 import { makeUnbanMemberRequest } from "@akasha-utils/chat-payload-builder-client";
+import { ChatErrorNumber } from "@common/chat-payloads";
+import type { ChatBanDetailEntry } from "@common/chat-payloads";
+import { Avatar } from "@components/Avatar";
+import { usePublicProfile } from "@hooks/useProfile";
+import { NickBlock } from "./ProfileItem";
 import { useWebSocket } from "@akasha-utils/react/websocket-hook";
 import { ChatClientOpcode } from "@common/chat-opcodes";
-import { ChatBanDetailEntry, ChatErrorNumber } from "@common/chat-payloads";
-import { Avatar } from "@components/Avatar";
 import { handleChatError } from "@components/Chat/handleChatError";
-import { usePublicProfile } from "@hooks/useProfile";
 
 export function ProfileItemBlocked({
     className,
     selected,
-    detail,
-    children,
+    entry,
     onClick,
 }: React.PropsWithChildren<{
     className?: string | undefined;
-    detail: ChatBanDetailEntry;
+    entry: ChatBanDetailEntry;
     selected: boolean;
     onClick: React.MouseEventHandler;
 }>) {
-    const profile = usePublicProfile(detail.accountId);
+    const profile = usePublicProfile(entry.accountId);
     const { sendPayload } = useWebSocket(
         "chat",
         ChatClientOpcode.UNBAN_MEMBER_RESULT,
         (_, buf) => {
             const [errno] = handleUnbanMemberResult(buf);
             if (errno === ChatErrorNumber.SUCCESS) {
-                alert("차단이 해제되었습니다.  ");
+                alert("차단을 해제했습니다.");
             } else {
                 handleChatError(errno);
             }
@@ -44,14 +45,14 @@ export function ProfileItemBlocked({
                 >
                     <div className="relative flex items-center justify-center">
                         <Avatar
-                            accountUUID={detail.accountId}
+                            accountUUID={entry.accountId}
                             className="w-6"
                             privileged={true}
                         />
                     </div>
                     <div className="w-full overflow-hidden">
                         <div className="relative w-full overflow-hidden whitespace-nowrap font-sans text-base font-bold leading-none tracking-normal text-gray-50 transition-all ease-linear group-hover/profile:-translate-x-[150%] group-hover/profile:overflow-visible group-hover/profile:delay-300 group-hover/profile:duration-[5000ms]">
-                            {profile?.nickName ?? "loading..."}
+                            <NickBlock profile={profile} />
                         </div>
                     </div>
                 </div>
@@ -59,14 +60,14 @@ export function ProfileItemBlocked({
                     <div className="flex w-full flex-col gap-2 p-2">
                         <div className="flex w-full flex-col rounded px-2">
                             <span className="text-gray-50">차단 사유</span>
-                            {detail.memo}
+                            {entry.memo}
                             <span className="text-gray-50">기간</span>
-                            {detail.expireTimestamp?.toString() ?? "unlimited"}
+                            {entry.expireTimestamp?.toString()}
                         </div>
                         <button
                             onClick={(event) => {
                                 event.preventDefault();
-                                sendPayload(makeUnbanMemberRequest(detail.id));
+                                sendPayload(makeUnbanMemberRequest(entry.id));
                             }}
                             className="w-full rounded bg-red-500 p-2 text-sm"
                         >
