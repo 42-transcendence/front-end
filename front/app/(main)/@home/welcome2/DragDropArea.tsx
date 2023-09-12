2e96fd67474c8d71c0d326b2cc3b38a5177906bb
chanhpar <chanhpar@student.42seoul.kr>
2023-09-12 11:25:57 +0900

    DragDrop 할때 이펙트 추가. 덤으로 z-index도 추가

--- front/app/(main)/@home/welcome2/DragDropArea.tsx
+++ front/app/(main)/@home/welcome2/DragDropArea.tsx
@@ -3,7 +3,8 @@ export function DragDropArea({
 }: {
     handleFiles: (files: FileList | null) => void;
 }) {
-    // TODO: drag, dragover에서 영역 비쥬얼 표시
+    const dragEffect = "bg-red-500";
+
     const handleDrag: React.DragEventHandler<HTMLDivElement> = (e) => {
         e.preventDefault();
     };
@@ -12,18 +13,31 @@ export function DragDropArea({
         e.preventDefault();
     };
 
+    const handleDragEnter: React.DragEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault();
+        const target = e.target as HTMLDivElement;
+        target.classList.add(dragEffect);
+    };
+
+    const handleDragLeave: React.DragEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault();
+        const target = e.target as HTMLDivElement;
+        target.classList.remove(dragEffect);
+    };
+
     const handleDrop: React.DragEventHandler<HTMLDivElement> = (e) => {
         e.preventDefault();
         handleFiles(e.dataTransfer.files);
     };
 
-    // TODO: 현재 DragDropArea의 z index가 밀려서 드래그드롭 안되는듯
-    //      ImageViewer 가리지 않으면서 drop event는 되게 하는 방법 있는지
+    // TODO: 그냥 z-20 넣어버렸습니다
     return (
         <div
-            className="absolute flex h-full w-full flex-col justify-center text-center"
+            className="absolute z-20 flex h-full w-full flex-col justify-center text-center"
             onDrag={handleDrag}
             onDragOver={handleDragOver}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
             onDrop={handleDrop}
         >
             파일 선택
