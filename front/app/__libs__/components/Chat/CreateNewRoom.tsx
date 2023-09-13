48259b6458eda3e313098d62433ba3de0e53fb73
chanhpar <chanhpar@student.42seoul.kr>
2023-09-13 23:29:11 +0900

    CreateNewRoom 할때 방 설정하는 토글버튼들 컴포넌트로 빼냄. data-* pseudo class로 되어있던거 삼항연산자 형식으로 수정. 아직 toggle button 내부 라벨에 있는 data-checked 속성을 없애진 않음

--- front/app/__libs__/components/Chat/CreateNewRoom.tsx
+++ front/app/__libs__/components/Chat/CreateNewRoom.tsx
@@ -133,101 +133,22 @@ export function CreateNewRoom() {
                         />
 
                         <div className="flex flex-col">
-                            <ToggleButton
-                                id="private"
-                                name="isPrivate"
-                                checked={privateChecked}
-                                setChecked={setPrivateChecked}
-                                bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
-                                icon={
-                                    <Icon.Lock
-                                        width={56}
-                                        height={56}
-                                        className="rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors group-data-[checked=true]:bg-secondary group-data-[checked=true]:text-gray-50/80"
-                                    />
-                                }
-                            >
-                                <div>
-                                    <p className="relative text-sm group-data-[checked=true]:hidden">
-                                        공개
-                                    </p>
-                                    <p className="relative hidden text-sm group-data-[checked=true]:block">
-                                        비공개
-                                    </p>
-                                </div>
-                            </ToggleButton>
-
-                            <ToggleButton
-                                id="secret"
-                                name="isSecret"
-                                checked={secretChecked}
-                                setChecked={setSecretChecked}
-                                bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
-                                icon={
-                                    <Icon.Key
-                                        width={56}
-                                        height={56}
-                                        className="shrink-0 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors group-data-[checked=true]:bg-secondary group-data-[checked=true]:text-gray-50/80"
-                                    />
-                                }
-                            >
-                                <div className="flex flex-col gap-1">
-                                    <div className="justify-center text-sm transition-all">
-                                        비밀번호
-                                    </div>
-                                    <div className="relative hidden h-full flex-col items-start justify-end gap-1 text-sm group-data-[checked=true]:flex">
-                                        <TextField
-                                            type="new-password"
-                                            name="password"
-                                            placeholder="비밀번호 입력"
-                                            className="bg-black/30 px-3 py-1 placeholder-gray-500/30"
-                                            value={password}
-                                            onChange={(event) =>
-                                                setPassword(event.target.value)
-                                            }
-                                        />
-                                    </div>
-                                </div>
-                            </ToggleButton>
-
-                            <ToggleButton
-                                id="limit"
-                                name="isLimit"
-                                checked={limitChecked}
-                                setChecked={setLimitChecked}
-                                bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
-                                icon={
-                                    <Icon.MembersFilled
-                                        width={56}
-                                        height={56}
-                                        className="shrink-0 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors group-data-[checked=true]:bg-secondary group-data-[checked=true]:text-gray-50/80"
-                                    />
-                                }
-                            >
-                                <div className="flex flex-col gap-1">
-                                    <h2 className="items-end justify-center text-sm transition-all">
-                                        인원제한
-                                    </h2>
-                                    <div className="relative hidden h-full flex-col items-start justify-end gap-1 text-sm group-data-[checked=true]:flex">
-                                        <TextField
-                                            type="number"
-                                            name="limit"
-                                            disabled={!limitChecked}
-                                            min={1}
-                                            max={MAX_CHAT_MEMBER_CAPACITY}
-                                            placeholder="최대인원 입력"
-                                            className="bg-black/30 px-3 py-1 placeholder-gray-500/30"
-                                            value={limit}
-                                            onChange={(event) =>
-                                                setLimit(
-                                                    Number(event.target.value),
-                                                )
-                                            }
-                                        />
-                                    </div>
-                                </div>
-                            </ToggleButton>
-
+                            <SetIsPrivateToggle
+                                privateChecked={privateChecked}
+                                setPrivateChecked={setPrivateChecked}
+                            />
+                            <SetIsSecretToggle
+                                secretChecked={secretChecked}
+                                setSecretChecked={setSecretChecked}
+                                password={password}
+                                setPassword={setPassword}
+                            />
+                            <SetLimitToggle
+                                limitChecked={limitChecked}
+                                setLimitChecked={setLimitChecked}
+                                limit={limit}
+                                setLimit={setLimit}
+                            />
                             <InviteFriendToggle
                                 checked={inviteChecked}
                                 setChecked={setInviteChecked}
@@ -235,13 +156,154 @@ export function CreateNewRoom() {
                         </div>
                     </div>
                 </div>
-
                 <ButtonOnRight
                     buttonText="만들기"
                     className="relative flex rounded-lg bg-gray-700/80 p-3 text-lg outline-none  focus-within:outline-primary/70 group-valid:bg-green-500/50 hover:group-valid:bg-green-400/50 hover:group-valid:text-white active:group-valid:bg-green-300/50 active:group-valid:text-white"
                 />
             </div>
         </form>
+    );
+}
+
+function SetIsPrivateToggle({
+    privateChecked,
+    setPrivateChecked,
+}: {
+    privateChecked: boolean;
+    setPrivateChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+    return (
+        <ToggleButton
+            id="private"
+            name="isPrivate"
+            checked={privateChecked}
+            setChecked={setPrivateChecked}
+            bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
+            icon={
+                <Icon.Lock
+                    width={56}
+                    height={56}
+                    className={`rounded-xl p-4 transition-colors ${
+                        privateChecked
+                            ? "bg-secondary text-gray-50/80"
+                            : "bg-gray-700/80 text-gray-50/50"
+                    }`}
+                />
+            }
+        >
+            <div>
+                <p className="relative text-sm">
+                    {privateChecked ? "비공개" : "공개"}
+                </p>
+            </div>
+        </ToggleButton>
+    );
+}
+
+function SetIsSecretToggle({
+    secretChecked,
+    setSecretChecked,
+    password,
+    setPassword,
+}: {
+    secretChecked: boolean;
+    setSecretChecked: React.Dispatch<React.SetStateAction<boolean>>;
+    password: string;
+    setPassword: React.Dispatch<React.SetStateAction<string>>;
+}) {
+    return (
+        <ToggleButton
+            id="secret"
+            name="isSecret"
+            checked={secretChecked}
+            setChecked={setSecretChecked}
+            bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
+            icon={
+                <Icon.Key
+                    width={56}
+                    height={56}
+                    className={`shrink-0 rounded-xl p-4 transition-colors ${
+                        secretChecked
+                            ? "bg-secondary text-gray-50/80"
+                            : "bg-gray-700/80 text-gray-50/50"
+                    }`}
+                />
+            }
+        >
+            <div className="flex flex-col gap-1">
+                <div className="justify-center text-sm transition-all">
+                    비밀번호
+                </div>
+                {secretChecked && (
+                    <div className="relative flex h-full flex-col items-start justify-end gap-1 text-sm">
+                        <TextField
+                            type="new-password"
+                            name="password"
+                            placeholder="비밀번호 입력"
+                            className="bg-black/30 px-3 py-1 placeholder-gray-500/30"
+                            value={password}
+                            onChange={(event) =>
+                                setPassword(event.target.value)
+                            }
+                        />
+                    </div>
+                )}
+            </div>
+        </ToggleButton>
+    );
+}
+
+function SetLimitToggle({
+    limitChecked,
+    setLimitChecked,
+    limit,
+    setLimit,
+}: {
+    limitChecked: boolean;
+    setLimitChecked: React.Dispatch<React.SetStateAction<boolean>>;
+    limit: number;
+    setLimit: React.Dispatch<React.SetStateAction<number>>;
+}) {
+    return (
+        <ToggleButton
+            id="limit"
+            name="isLimit"
+            checked={limitChecked}
+            setChecked={setLimitChecked}
+            bgClassName="gap-3 rounded p-3 hover:bg-gray-500/30"
+            icon={
+                <Icon.MembersFilled
+                    width={56}
+                    height={56}
+                    className={`shrink-0 rounded-xl p-4 transition-colors ${
+                        limitChecked
+                            ? "bg-secondary text-gray-50/80"
+                            : "bg-gray-700/80 text-gray-50/50"
+                    }`}
+                />
+            }
+        >
+            <div className="flex flex-col gap-1">
+                <h2 className="items-end justify-center text-sm transition-all">
+                    인원제한
+                </h2>
+                <div className="relative hidden h-full flex-col items-start justify-end gap-1 text-sm group-data-[checked=true]:flex">
+                    <TextField
+                        type="number"
+                        name="limit"
+                        disabled={!limitChecked}
+                        min={1}
+                        max={MAX_CHAT_MEMBER_CAPACITY}
+                        placeholder="최대인원 입력"
+                        className="bg-black/30 px-3 py-1 placeholder-gray-500/30"
+                        value={limit}
+                        onChange={(event) =>
+                            setLimit(Number(event.target.value))
+                        }
+                    />
+                </div>
+            </div>
+        </ToggleButton>
     );
 }
 
@@ -259,14 +321,16 @@ function InviteFriendToggle({
             <label
                 ref={ref}
                 htmlFor="sectionHeader"
-                data-checked={checked}
-                data-sticky={isSticky}
-                className="group sticky -top-1 z-10 flex h-fit w-full flex-row items-center gap-3 rounded p-3 transition-colors hover:bg-gray-500/30 hover:text-gray-50/80 data-[checked=true]:data-[sticky=true]:bg-secondary data-[checked=true]:data-[sticky=true]:duration-0"
+                className={`group sticky -top-1 z-10 flex h-fit w-full flex-row items-center gap-3 rounded p-3 transition-colors hover:bg-gray-500/30 hover:text-gray-50/80 ${
+                    checked && isSticky ? "bg-secondary duration-0" : ""
+                }`}
             >
                 <Icon.Invite
                     width={56}
                     height={56}
-                    className="shrink-0 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors group-data-[checked=true]:bg-secondary group-data-[checked=true]:text-gray-50/80"
+                    className={`shrink-0 rounded-xl bg-gray-700/80 p-4 text-gray-50/50 transition-colors ${
+                        checked && "bg-secondary text-gray-50/80"
+                    }`}
                 />
                 <input
                     onChange={(event) => setChecked(event.target.checked)}
@@ -275,7 +339,7 @@ function InviteFriendToggle({
                     id="sectionHeader"
                     className="hidden"
                 />
-                <p className="px-1 text-sm group-data-[checked=true]:text-gray-50/80">
+                <p className={`px-1 text-sm ${checked && "text-gray-50/80"}`}>
                     초대 상대 선택
                 </p>
             </label>
