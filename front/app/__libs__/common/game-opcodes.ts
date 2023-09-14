452aef32be9dea2aa23853f73de5c16198f29119
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-14 18:21:13 +0900

    update

--- front/app/__libs__/common/game-opcodes.ts
+++ front/app/__libs__/common/game-opcodes.ts
@@ -1,19 +1,48 @@
+// export const enum GameServerOpcode {
+//   HANDSHAKE,
+//   CREATE,
+//   START,
+//   JOIN,
+//   FRAME
+// }
+//
+// export const enum GameClientOpcode {
+//   INITIALIZE,
+//   ACCEPT,
+//   REJECT,
+//   START,
+//   RESYNC,
+//   SYNC,
+//   WIN,
+//   LOSE,
+//   DRAW
+// }
 export const enum GameServerOpcode {
-  HANDSHAKE,
-  CREATE,
-  START,
-  JOIN,
-  FRAME
+    HANDSHAKE_GAME,
+    SELECT_CHAR,
+    SELECT_SPEC,
+    CHANGE_TEAM,
+    READY_STATE,
+    SYNCHRONIZE_REQUEST,
+    RESYNCHRONIZE_RESULT,
+
+    HANDSHAKE_MATCHMAKE = 0x100,
 }
 
 export const enum GameClientOpcode {
-  INITIALIZE,
-  ACCEPT,
-  REJECT,
-  START,
-  RESYNC,
-  SYNC,
-  WIN,
-  LOSE,
-  DRAW
+    GAME_ROOM,
+    GAME_FAILED,
+    ENTER_MEMBER,
+    UPDATE_MEMBER,
+    LEAVE_MEMBER,
+    SYNCHRONIZE_RESULT,
+    RESYNCHRONIZE_REQUEST,
+    UPDATE_GAME,
+    GAME_INTERMEDIATE_RESULT,
+    GAME_FINAL_RESULT,
+    ACHIEVEMENT,
+
+    ENQUEUED = 0x100,
+    INVITATION,
+    MATCHMAKE_FAILED,
 }
--- front/app/__libs__/components/NavigationBar/MatchMaker.tsx
+++ front/app/__libs__/components/NavigationBar/MatchMaker.tsx
@@ -1,4 +1,5 @@
 export function MatchMakerPanel() {
+    const timer = useTimer;
     return (
         <div className="relative flex w-full items-center justify-center">
             <span>게임 찾는 중</span>
