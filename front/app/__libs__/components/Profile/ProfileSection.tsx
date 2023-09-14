3ef161ed9cbb9df7e1d227bea739a5ed76393ac2
chanhpar <chanhpar@student.42seoul.kr>
2023-09-15 02:37:04 +0900

    ProfileSection 에 친구 추가삭제기능을 넣어봄

--- front/app/__libs__/components/Profile/ProfileSection.tsx
+++ front/app/__libs__/components/Profile/ProfileSection.tsx
@@ -2,9 +2,14 @@ import { EditPanelVisibilityAtom } from "@atoms/ProfileAtom";
 import { Avatar } from "@components/Avatar";
 import type { Relationship } from "@components/ContextMenu";
 import { useCurrentAccountUUID } from "@hooks/useCurrent";
-import { useProtectedProfile } from "@hooks/useProfile";
+import { useProtectedProfile, usePublicProfile } from "@hooks/useProfile";
 import { useSetAtom } from "jotai";
 import { Icon } from "@components/ImageLibrary";
+import {
+    makeAddFriendRequest,
+    makeDeleteFriendRequest,
+} from "@akasha-utils/chat-payload-builder-client";
+import { useWebSocket } from "@akasha-utils/react/websocket-hook";
 
 export function ProfileSection({ accountUUID }: { accountUUID: string }) {
     const profile = useProtectedProfile(accountUUID);
@@ -36,19 +41,47 @@ export function ProfileSection({ accountUUID }: { accountUUID: string }) {
                         </h2>
                     </div>
                 </div>
-                <RelationshipButton relationship={relationship} />
+                <RelationshipButton
+                    relationship={relationship}
+                    targetAccountUUID={accountUUID}
+                />
             </div>
         </div>
     );
 }
 
-function RelationshipButton({ relationship }: { relationship: Relationship }) {
+function RelationshipButton({
+    relationship,
+    targetAccountUUID,
+}: {
+    relationship: Relationship;
+    targetAccountUUID: string;
+}) {
     const showEditPanel = useSetAtom(EditPanelVisibilityAtom);
+    const targetProfile = usePublicProfile(targetAccountUUID);
+    const { sendPayload } = useWebSocket("chat", []);
+
+    const nickName = targetProfile?.nickName ?? "fallback";
+
+    const addfriend = () => {
+        const buf = makeAddFriendRequest(targetAccountUUID, "", 0b11111111);
+        sendPayload(buf);
+    };
+    const deletefriend = () => {
+        if (
+            confirm(
+                `진짜로 정말로 [${nickName}]님을 친구 목록에서 삭제하실건가요...?`,
+            )
+        ) {
+            sendPayload(makeDeleteFriendRequest(targetAccountUUID));
+        }
+    };
 
     switch (relationship) {
         case "myself":
             return (
                 <button
+                    type="button"
                     onClick={() => showEditPanel((value) => !value)}
                     className="relative flex h-8 items-center justify-center rounded-xl bg-secondary p-2 lg:w-full"
                 >
@@ -57,13 +90,21 @@ function RelationshipButton({ relationship }: { relationship: Relationship }) {
             );
         case "friend":
             return (
-                <button className="relative flex h-8 items-center justify-center rounded-xl bg-green-500 p-2 lg:w-full">
+                <button
+                    type="button"
+                    onClick={() => deletefriend()}
+                    className="relative flex h-8 items-center justify-center rounded-xl bg-green-500 p-2 lg:w-full"
+                >
                     <Icon.Check width={16} height={16} />
                 </button>
             );
         case "stranger":
             return (
-                <button className="relative flex h-8 items-center justify-center rounded-xl bg-gray-500 p-2 lg:w-full">
+                <button
+                    type="button"
+                    onClick={() => addfriend()}
+                    className="relative flex h-8 items-center justify-center rounded-xl bg-gray-500 p-2 lg:w-full"
+                >
                     <Icon.Plus width={16} height={16} />
                 </button>
             );
