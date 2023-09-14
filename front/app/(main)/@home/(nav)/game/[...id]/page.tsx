a7bf21959be33f3d9d7a43b223b33efe1f01d97e
chanhpar <chanhpar@student.42seoul.kr>
2023-09-15 08:48:27 +0900

    game page 만드는 중...

--- front/app/(main)/@home/(nav)/game/[...id]/page.tsx
+++ front/app/(main)/@home/(nav)/game/[...id]/page.tsx
@@ -2,57 +2,68 @@
 
 import { useWebSocket } from "@akasha-utils/react/websocket-hook";
 import { InvitationAtom } from "@atoms/GameAtom";
+import {
+    handleEnterMember,
+    handleGameFailedPayload,
+    handleGameRoom,
+    handleLeaveMember,
+    handleUpdateMember,
+} from "@common/game-gateway-helper-client";
 import { GameClientOpcode } from "@common/game-opcodes";
 import { makeGameHandshake } from "@common/game-payload-builder-client";
+import { GameRoomEnterResult } from "@common/game-payloads";
 import { GameLobby } from "@components/Game/GameLobby";
+import { handleGameError } from "@components/Game/handleGameError";
 import { useGamePlayConnector } from "@hooks/useGameWebSocketConnector";
 import { useAtomValue } from "jotai";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 export default function GamePage() {
     const invitationAtom = useAtomValue(InvitationAtom);
+    const router = useRouter();
 
     useGamePlayConnector(
         useMemo(() => makeGameHandshake(invitationAtom), [invitationAtom]),
     );
 
-    useWebSocket("game", undefined, (opcode, _buffer) => {
-        switch (opcode) {
-            case GameClientOpcode.GAME_ROOM: {
-                break;
+    const { sendPayload } = useWebSocket(
+        "game",
+        undefined,
+        (opcode, buffer) => {
+            switch (opcode) {
+                case GameClientOpcode.GAME_ROOM: {
+                    const [props, params, members, ladder] =
+                        handleGameRoom(buffer);
+                    break;
+                }
+                case GameClientOpcode.GAME_FAILED: {
+                    const errno = handleGameFailedPayload(buffer);
+                    if (errno !== GameRoomEnterResult.SUCCESS) {
+                        handleGameError(errno);
+                        router.push("/");
+                        return;
+                    } else {
+                    }
+                    break;
+                }
+                case GameClientOpcode.ENTER_MEMBER: {
+                    const [accountId, character, specification, team, ready] =
+                        handleEnterMember(buffer);
+                    break;
+                }
+                case GameClientOpcode.UPDATE_MEMBER: {
+                    const [accountId, character, specification, team, ready] =
+                        handleUpdateMember(buffer);
+                    break;
+                }
+                case GameClientOpcode.LEAVE_MEMBER: {
+                    const accountId = handleLeaveMember(buffer);
+                    break;
+                }
             }
-            case GameClientOpcode.GAME_FAILED: {
-                break;
-            }
-            case GameClientOpcode.ENTER_MEMBER: {
-                break;
-            }
-            case GameClientOpcode.UPDATE_MEMBER: {
-                break;
-            }
-            case GameClientOpcode.LEAVE_MEMBER: {
-                break;
-            }
-            case GameClientOpcode.SYNCHRONIZE_RESULT: {
-                break;
-            }
-            case GameClientOpcode.RESYNCHRONIZE_REQUEST: {
-                break;
-            }
-            case GameClientOpcode.UPDATE_GAME: {
-                break;
-            }
-            case GameClientOpcode.GAME_INTERMEDIATE_RESULT: {
-                break;
-            }
-            case GameClientOpcode.GAME_FINAL_RESULT: {
-                break;
-            }
-            case GameClientOpcode.ACHIEVEMENT: {
-                break;
-            }
-        }
-    });
+        },
+    );
 
     return <GameLobby />;
 }
