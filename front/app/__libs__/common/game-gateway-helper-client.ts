2aa6e8f7a0f62b30ccbb381a43257357822024ee
jkong <jkong@student.42seoul.kr>
2023-09-14 21:39:35 +0900

    handler

--- front/app/__libs__/common/game-gateway-helper-client.ts
+++ front/app/__libs__/common/game-gateway-helper-client.ts
@@ -1,1 +1,29 @@
-//TODO: 작성
+import type { ByteBuffer } from "@akasha-lib";
+import type { BattleField, GameMode, MatchmakeFailedReason } from "./game-payloads";
+
+export function handleEnqueueAlert(
+    payload: ByteBuffer,
+): [
+    battleField: BattleField,
+    gameMode: GameMode,
+    limit: number,
+    fair: boolean,
+] {
+    const battleField = payload.read4Unsigned();
+    const gameMode = payload.read1();
+    const limit = payload.read2();
+    const fair = payload.readBoolean();
+    return [battleField, gameMode, limit, fair];
+}
+
+export function handleInvitationPayload(payload: ByteBuffer): string {
+    const invitation = payload.readString();
+    return invitation;
+}
+
+export function handleMatchmakeFailed(
+    payload: ByteBuffer,
+): MatchmakeFailedReason {
+    const reason = payload.read1();
+    return reason;
+}
