14ed87dd3a05cb2f80db8ec787dd40f78250625f
chanhpar <chanhpar@student.42seoul.kr>
2023-09-15 12:11:14 +0900

    게임로비 코드 복사기능 추가

--- front/app/__libs__/components/Game/GameLobby.tsx
+++ front/app/__libs__/components/Game/GameLobby.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCurrentAccountUUID } from "@hooks/useCurrent";
 import { Avatar } from "@components/Avatar";
 import { GameMemberAtom, GameRoomPropsAtom } from "@atoms/GameAtom";
@@ -5,6 +7,8 @@ import { useAtomValue } from "jotai";
 import { useWebSocket } from "@akasha-utils/react/websocket-hook";
 import { ByteBuffer } from "@akasha-lib";
 import { GameServerOpcode } from "@common/game-opcodes";
+import { useEffect, useState } from "react";
+import { HOST } from "@utils/constants";
 
 function makeUpdateMemberRequest(ready: boolean) {
     const buf = ByteBuffer.createWithOpcode(GameServerOpcode.READY_STATE);
@@ -16,6 +20,7 @@ export function GameLobby() {
     const currentAccountUUID = useCurrentAccountUUID();
     const members = useAtomValue(GameMemberAtom);
     const gameRoomProps = useAtomValue(GameRoomPropsAtom);
+    const [roomCode, setRoomCode] = useState("");
 
     const { sendPayload } = useWebSocket("game", []);
 
@@ -23,13 +28,36 @@ export function GameLobby() {
         sendPayload(makeUpdateMemberRequest(true));
     };
 
+    useEffect(() => {
+        if (gameRoomProps !== null && gameRoomProps.code !== null) {
+            setRoomCode(gameRoomProps.code);
+        }
+    }, [gameRoomProps]);
+
     if (gameRoomProps === null) {
         return null;
     }
 
+    const copyGameRoomCode = () => {
+        const code = gameRoomProps.code;
+        if (code === null) {
+            alert("빠른 대전은 방 공유가 불가능합니다");
+            return;
+        }
+
+        navigator.clipboard
+            .writeText(`https://${HOST}/game/${code}`)
+            .then(() => setRoomCode("복사 완료!"))
+            .catch(() => setRoomCode("복사 실패"));
+
+        setTimeout(() => setRoomCode(code), 1000);
+    };
+
     return (
         <div>
-            {`id: ${gameRoomProps.id} code: ${gameRoomProps.code}`}
+            <button type="button" onClick={() => copyGameRoomCode()}>
+                방 코드: {roomCode}
+            </button>
             <div className="h-1/2 w-1/2">
                 {members.map((member) => (
                     <div
