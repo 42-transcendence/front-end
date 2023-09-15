0492f5fe26289f446bd32c82388b926dd74fc00e
chanhpar <chanhpar@student.42seoul.kr>
2023-09-15 14:51:06 +0900

    game lobby WIP

--- front/app/__libs__/components/Game/GameLobby.tsx
+++ front/app/__libs__/components/Game/GameLobby.tsx
@@ -9,9 +9,27 @@ import { useCallback, useEffect } from "react";
 import { HOST } from "@utils/constants";
 import { makeReadyStateRequest } from "@akasha-utils/game-payload-builder-clients";
 import { useCopyText } from "@hooks/useCopyText";
+import type { GameMemberParams } from "@common/game-payloads";
+import { partition } from "@utils/collections";
+
+type GameTeamParams = {
+    id: number;
+    name: string;
+    members: GameMemberParams[];
+};
+
+// function useAudio(src: string) {
+//     useEffect(() => {
+//         const audio = new Audio(src);
+//         audio.onload = () => audio.play();
+//
+//         return () => audio.pause();
+//     }, [src]);
+// }
+
+const teamName = ["A", "B"];
 
 export function GameLobby() {
-    const currentAccountUUID = useCurrentAccountUUID();
     const members = useAtomValue(GameMemberAtom);
     const gameRoomProps = useAtomValue(GameRoomPropsAtom);
     const [roomCode, setRoomCode, copyRoomCode] = useCopyText({
@@ -38,34 +56,61 @@ export function GameLobby() {
         return null;
     }
 
+    const [teamA, teamB] = partition(
+        members,
+        (member: GameMemberParams) => member.team === 0,
+    ).map((e, index) => ({ id: index, name: teamName[index], members: e }));
+
     return (
         <div>
             <button type="button" onClick={() => copyRoomCode()}>
                 방 주소: {roomCode}
             </button>
-            <div className="h-1/2 w-1/2">
-                {members.map((member) => (
-                    <div
-                        key={member.accountId}
-                        className={`${
-                            member.accountId === currentAccountUUID ? "" : ""
-                        }`}
-                    >
-                        <Avatar
-                            accountUUID={member.accountId}
-                            className="relative h-12 w-12"
-                            privileged={false}
-                        />
-                        <button
-                            className="border-red-400"
-                            onClick={() => toggleReady(!member.ready)}
-                            disabled={currentAccountUUID !== member.accountId}
-                        >
-                            {member.ready ? "ready" : "unready"}
-                        </button>
-                    </div>
+            <section className="flex flex-row">
+                {[teamA, teamB].map((team) => (
+                    <TeamPanel
+                        key={team.id}
+                        team={team}
+                        toggleReady={toggleReady}
+                    />
                 ))}
-            </div>
+            </section>
+        </div>
+    );
+}
+
+function TeamPanel({
+    team,
+    toggleReady,
+}: {
+    team: GameTeamParams;
+    toggleReady: (x: boolean) => void;
+}) {
+    const currentAccountUUID = useCurrentAccountUUID();
+    return (
+        <div className="flex flex-col bg-primary/30">
+            {`team ${team.name}`}
+            {team.members.map((member) => (
+                <div
+                    key={member.accountId}
+                    className={`${
+                        member.accountId === currentAccountUUID ? "" : ""
+                    }`}
+                >
+                    <Avatar
+                        accountUUID={member.accountId}
+                        className="relative h-12 w-12"
+                        privileged={false}
+                    />
+                    <button
+                        className="border-red-400"
+                        onClick={() => toggleReady(!member.ready)}
+                        disabled={currentAccountUUID !== member.accountId}
+                    >
+                        {member.ready ? "ready" : "unready"}
+                    </button>
+                </div>
+            ))}
         </div>
     );
 }
