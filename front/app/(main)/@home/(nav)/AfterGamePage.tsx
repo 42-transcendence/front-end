0f83bc9816e897b6ae3874fe24fd6f8c633c3412
chanhpar <chanhpar@student.42seoul.kr>
2023-09-16 16:29:17 +0900

    AfterGamePage WIP

--- front/app/(main)/@home/(nav)/AfterGamePage.tsx
+++ front/app/(main)/@home/(nav)/AfterGamePage.tsx
@@ -1,28 +1,42 @@
 "use client";
 
+const mockData = {
+    team: {
+        0: {
+            total: 3,
+            set: [8, 15, 11, 4, 15, 15, 9],
+        },
+        1: {
+            total: 4,
+            set: [15, 14, 15, 15, 9, 4, 15],
+        },
+    },
+    members: [
+        { accountId: "asdf", team: 0, ratingBefore: 1000, ratingAfter: 984 },
+        { accountId: "zxcv", team: 0, ratingBefore: 1000, ratingAfter: 985 },
+        { accountId: "qwer", team: 1, ratingBefore: 1238, ratingAfter: 1004 },
+        { accountId: "hjkl", team: 1, ratingBefore: 1321, ratingAfter: 1005 },
+    ],
+    time: 538,
+};
+
 export function AfterGamePage() {
-    const data = {
-        team: {
-            0: {
-                total: 3,
-                set: [8, 15, 11, 4, 15, 15, 9],
-            },
-            1: {
-                total: 4,
-                set: [15, 14, 15, 15, 9, 4, 15],
-            },
-        },
-        member: {
-            asdf: { team: 0, ratingBefore: 1000, ratingAfter: 984 },
-            zxcv: { team: 0, ratingBefore: 1000, ratingAfter: 985 },
-            qwer: { team: 1, ratingBefore: 1238, ratingAfter: 1004 },
-            hjkl: { team: 1, ratingBefore: 1321, ratingAfter: 1005 },
-        },
-        time: 538,
-    };
+    const gameResult = mockData;
 
     const currentAccountUUID = "asdf";
-    const myTeam = data.member[currentAccountUUID].team;
-    const isWin = data.team;
-    return <div className="">{<span>승리</span>}</div>;
+    const myMember = gameResult.members.find(
+        (member) => member.accountId === currentAccountUUID,
+    );
+
+    if (myMember === undefined) {
+        // 이거 뭐지
+        return <div>loading</div>;
+    }
+    //hdoo님 이런 식은 어떤가요
+    const myTeam = myMember.team;
+    const winTeam = gameResult.team[0].total > gameResult.team[1].total ? 0 : 1;
+    const isWin = myTeam === winTeam;
+    return (
+        <div className="">{isWin ? <span>승리</span> : <span>패배</span>}</div>
+    );
 }
