cc62e9161f905520a651113bb15b0e23dcece99d
chanhpar <chanhpar@student.42seoul.kr>
2023-09-16 17:27:22 +0900

    profile modal isOpen이 atom으로 되어있던걸 react useState으로 바꿈

--- front/app/__libs__/components/NavigationBar/ProfileButton.tsx
+++ front/app/__libs__/components/NavigationBar/ProfileButton.tsx
@@ -3,13 +3,12 @@
 import { Avatar } from "@components/Avatar";
 import { useCurrentAccountUUID } from "@hooks/useCurrent";
 import { Dialog } from "@headlessui/react";
-import { ProfileModalIsOpen } from "@atoms/ProfileAtom";
-import { useAtom } from "jotai";
 import { ProfileModal } from "./ProfileModal";
+import { useState } from "react";
 
 export function ProfileButton() {
     const currentAccountUUID = useCurrentAccountUUID();
-    const [isOpen, setIsOpen] = useAtom(ProfileModalIsOpen);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <>
